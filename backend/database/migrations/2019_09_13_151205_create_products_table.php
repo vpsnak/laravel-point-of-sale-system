48c8afd025813c519f621dc39214f267cf711def
vpsnak <info.vpsnak@gmail.com>
2019-09-14 00:34:56 +0300

    db structure for sales and sale items, also added gitignore for editor files

--- backend/database/migrations/2019_09_13_151205_create_products_table.php
+++ backend/database/migrations/2019_09_13_151205_create_products_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateCustomersTable extends Migration
+class CreateProductsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,12 +13,14 @@ class CreateCustomersTable extends Migration
      */
     public function up()
     {
-        Schema::create('customers', function (Blueprint $table) {
+        Schema::create('products', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->string('email')->unique()->index();
-            $table->string('first_name', 100);
-            $table->string('last_name', 100);
-            $table->string('company_name', 100);
+            $table->string('sku')->unique()->index();
+            $table->string('name');
+            $table->string('photo_url');
+            $table->integer('quantity');
+            $table->decimal('price');
+            $table->softDeletes();
             $table->timestamps();
         });
     }
@@ -30,6 +32,6 @@ class CreateCustomersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('customers');
+        Schema::dropIfExists('products');
     }
 }
