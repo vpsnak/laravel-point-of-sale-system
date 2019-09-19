4a5675d2efff7c4b3dfb7d6a6e2edff06dbe2cfe
Nikos Papaioannou <nikoskalmah@hotmail.com>
2019-09-19 09:05:06 +0300

    side menu items  and pages created

---
+++ resources/js/components/pages/Users.vue
@@ -0,0 +1,9 @@
+<template>
+	<v-container fluid>
+      <v-row>
+         <v-col cols="3">
+           Users
+         </v-col>
+      </v-row>
+   </v-container>
+</template>
--- resources/js/routes.js
+++ resources/js/routes.js
@@ -1,7 +1,14 @@
 import Dashboard from "./components/pages/Dashboard";
 import Sales from "./components/pages/Sales";
-import NTest from "./components/pages/NTest";
+import Forms from "./components/pages/Forms";
+import Orders from "./components/pages/Orders";
+import Customers from "./components/pages/Customers";
+import Products from "./components/pages/Products";
+import Categories from "./components/pages/Categories";
+import Stores from "./components/pages/Stores";
+import Users from "./components/pages/Users";
 import CTest from "./components/pages/CTest";
+
 
 export default [
     {
@@ -15,9 +22,39 @@ export default [
         component: Sales
     },
     {
-        name: "testn",
-        path: "/nikos",
-        component: NTest
+        name: "Forms",
+        path: "/forms",
+        component: Forms
+    },
+    {
+        name: "Orders",
+        path: "/orders",
+        component: Orders
+    },
+    {
+        name: "Customers",
+        path: "/customers",
+        component: Customers
+    },
+    {
+        name: "Products",
+        path: "/products",
+        component: Products
+    },
+    {
+        name: "Categories",
+        path: "/categories",
+        component: Categories
+    },
+    {
+        name: "Stores",
+        path: "/stores",
+        component: Stores
+    },
+    {
+        name: "Users",
+        path: "/users",
+        component: Users
     },
     {
         name: "testc",
