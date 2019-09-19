9a01859ee908b0cb6f16c7c502c57dee55a142b5
vpsnak <info.vpsnak@gmail.com>
2019-09-19 20:07:50 +0300

    order and order product controller insert actions

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -2,7 +2,43 @@
 
 namespace App\Http\Controllers;
 
+use App\Order;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
 class OrderController extends Controller
 {
-    //
+    public function create(Request $request)
+    {
+        $response = $this->insert($request->all());
+
+        if ($response) {
+            return response(true, 200);
+        } else {
+            return response(false, 200);
+        }
+    }
+
+    public function insert($data)
+    {
+        $validatedData = Validator::make($data, [
+            'customer_id' => 'required|numeric|nullable',
+            'user_id' => 'required|numeric',
+            'discount_type' => 'required|string',
+            'discount' => 'required|string',
+            'shipping_type' => 'required|string',
+            'shipping_cost' => 'required|numeric',
+            'tax' => 'required|numeric',
+            'subtotal' => 'required|numeric',
+            'note' => 'required|string',
+        ]);
+
+        $insert = Order::create($validatedData);
+
+        if (!empty($insert)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
--- app/Http/Controllers/OrderProductController.php
+++ app/Http/Controllers/OrderProductController.php
@@ -2,7 +2,41 @@
 
 namespace App\Http\Controllers;
 
+use App\OrderProduct;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
 class OrderProductController extends Controller
 {
-    //
+    public function create(Request $request)
+    {
+        $response = $this->insert($request->all());
+
+        if ($response) {
+            return response(true, 200);
+        } else {
+            return response(false, 500);
+        }
+    }
+
+    public function insert($data)
+    {
+        $validatedData = Validator::make($data, [
+            'order_id' => 'required|numeric',
+            'name' => 'required|string',
+            'sku' => 'required|string',
+            'price' => 'required|numeric',
+            'qty' => 'required|string',
+            'discount_type' => 'required|string',
+            'discount' => 'required|numeric',
+        ]);
+
+        $insert = OrderProduct::create($validatedData);
+
+        if (!empty($insert)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
