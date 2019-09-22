58f69779bf0b43667dd110f04beb1fc7695fcc88
Nikos Papaioannou <nikoskalmah@hotmail.com>
2019-09-22 16:44:05 +0300

    CartList: search request for customers

--- resources/js/components/tables/CustomerTable.vue
+++ resources/js/components/tables/CustomerTable.vue
@@ -1,113 +1,118 @@
 <template>
-	<v-card>
-		<v-card-title>
-			Customers
-			<v-spacer></v-spacer>
-			<v-text-field v-model="search" append-icon="search" label="Search" single-line hide-details></v-text-field>
-		</v-card-title>
-		<v-data-table :headers="headers" :items="customerList" :search="search" :loading="loader">
-			<template v-slot:item.email="{item}">
-				<a :href="'mailto:' + item.email">{{ item.email }}</a>
-			</template>
-			<template v-slot:item.phone="{item}">
-				<a :href="'tel:' + item.phone">{{ item.phone }}</a>
-			</template>
-			<v-alert
-				slot="no-results"
-				:value="true"
-				color="error"
-				icon="warning"
-			>Your search for "{{ search }}" found no results.</v-alert>
-		</v-data-table>
-	</v-card>
+    <v-card>
+        <v-card-title>
+            Customers
+            <v-spacer></v-spacer>
+            <v-text-field
+                v-model="search"
+                append-icon="search"
+                label="Search"
+                single-line
+                hide-details
+            ></v-text-field>
+        </v-card-title>
+        <v-data-table :headers="headers" :items="customerList" :search="search" :loading="loader">
+            <template v-slot:item.email="{item}">
+                <a :href="'mailto:' + item.email">{{ item.email }}</a>
+            </template>
+            <template v-slot:item.phone="{item}">
+                <a :href="'tel:' + item.phone">{{ item.phone }}</a>
+            </template>
+            <v-alert
+                slot="no-results"
+                :value="true"
+                color="error"
+                icon="warning"
+            >Your search for "{{ search }}" found no results.</v-alert>
+        </v-data-table>
+    </v-card>
 </template>
 
 
 <script>
-	export default {
-		data() {
-			return {
-				loader: false,
-				disableFilters: false,
-				model: "customers",
-				keyword: "",
-				search: "",
-				headers: [
-					{
-						text: "Id",
-						value: "id"
-					},
-					{
-						text: "First name",
-						value: "first_name"
-					},
-					{
-						text: "Last name",
-						value: "last_name"
-					},
-					{
-						text: "E-mail",
-						value: "email",
-						sortable: false
-					},
-					{
-						text: "Phone",
-						value: "phone",
-						sortable: false
-					},
-					{
-						text: "Company name",
-						value: "company_name",
-						sortable: false
-					},
-					{
-						text: "Address",
-						value: "addresses[0].street",
-						sortable: false
-					}
-				]
-			};
-		},
-		mounted() {
-			this.getAllCustomers();
-		},
-		computed: {
-			customerList: {
-				get() {
-					return this.$store.state.customerList;
-				},
-				set(value) {
-					this.$store.state.customerList = value;
-				}
-			}
-		},
-		methods: {
-			applyFilter(filter) {},
-			initiateLoadingSearchResults(loading) {
-				if (loading) {
-					this.loader = true;
-					this.customerList = [];
-				} else {
-					this.loader = false;
-				}
-			},
-			getAllCustomers() {
-				this.initiateLoadingSearchResults(true);
+export default {
+    data() {
+        return {
+            loader: false,
+            disableFilters: false,
+            model: "customers",
+            search: "",
+            headers: [
+                {
+                    text: "Id",
+                    value: "id"
+                },
+                {
+                    text: "First name",
+                    value: "first_name"
+                },
+                {
+                    text: "Last name",
+                    value: "last_name"
+                },
+                {
+                    text: "E-mail",
+                    value: "email",
+                    sortable: false
+                },
+                {
+                    text: "Phone",
+                    value: "phone",
+                    sortable: false
+                },
+                {
+                    text: "Company name",
+                    value: "company_name",
+                    sortable: false
+                },
+                {
+                    text: "Address",
+                    value: "addresses[0].street",
+                    sortable: false
+                }
+            ]
+        };
+    },
+    mounted() {
+        this.getAllCustomers();
+    },
+    computed: {
+        customerList: {
+            get() {
+                return this.$store.state.customerList;
+            },
+            set(value) {
+                this.$store.state.customerList = value;
+            }
+        }
+    },
+    methods: {
+        applyFilter(filter) {},
+        initiateLoadingSearchResults(loading) {
+            if (loading) {
+                this.loader = true;
+                this.customerList = [];
+            } else {
+                this.loader = false;
+            }
+        },
+        getAllCustomers() {
+            this.initiateLoadingSearchResults(true);
 
-				let payload = {
-					model: "customers",
-					mutation: "setCustomerList"
-				};
-				this.$store
-					.dispatch("getAll", payload)
-					.then(result => {
-						this.initiateLoadingSearchResults(false);
-					})
-					.catch(error => {
-						this.initiateLoadingSearchResults(false);
-						console.log(error);
-					});
-			}
-		}
-	};
+            let payload = {
+                model: "customers",
+                mutation: "setCustomerList"
+            };
+            this.$store
+                .dispatch("getAll", payload)
+                .then(result => {
+                    this.initiateLoadingSearchResults(false);
+                })
+                .catch(error => {
+                    this.initiateLoadingSearchResults(false);
+                    console.log(error);
+                });
+        }
+    }
+};
 </script>
--- resources/js/components/tables/ProductTable.vue
+++ resources/js/components/tables/ProductTable.vue
@@ -1,106 +1,111 @@
 <template>
-	<v-card>
-		<v-card-title>
-			Products
-			<v-spacer></v-spacer>
-			<v-text-field v-model="search" append-icon="search" label="Search" single-line hide-details></v-text-field>
-		</v-card-title>
-		<v-data-table :headers="headers" :items="productList" :search="search" :loading="loader">
-			<v-alert
-				slot="no-results"
-				:value="true"
-				color="error"
-				icon="warning"
-			>Your search for "{{ search }}" found no results.</v-alert>
-		</v-data-table>
-	</v-card>
+    <v-card>
+        <v-card-title>
+            Products
+            <v-spacer></v-spacer>
+            <v-text-field
+                v-model="search"
+                append-icon="search"
+                label="Search"
+                single-line
+                hide-details
+            ></v-text-field>
+        </v-card-title>
+        <v-data-table :headers="headers" :items="productList" :search="search" :loading="loader">
+            <v-alert
+                slot="no-results"
+                :value="true"
+                color="error"
+                icon="warning"
+            >Your search for "{{ search }}" found no results.</v-alert>
+        </v-data-table>
+    </v-card>
 </template>
 
 
 <script>
-	export default {
-		data() {
-			return {
-				loader: false,
-				disableFilters: false,
-				model: "products",
-				keyword: "",
-				search: "",
-				headers: [
-					{
-						text: "Products",
-						align: "left",
-						sortable: false,
-						value: "name"
-					},
-					{ text: "Id", value: "id" },
-					{ text: "Sku", value: "sku" },
-					{ text: "Photo url", value: "photo_url" },
-					{ text: "Price", value: "price.amount" }
-				]
-			};
-		},
-		mounted() {
-			this.getAllProducts();
-		},
-		computed: {
-			productList: {
-				get() {
-					return this.$store.state.productList;
-				},
-				set(value) {
-					this.$store.state.productList = value;
-				}
-			}
-		},
-		methods: {
-			initiateLoadingSearchResults(loading) {
-				if (loading) {
-					this.loader = true;
-					this.disableFilters = true;
-					this.productList = [];
-				} else {
-					this.loader = false;
-					this.disableFilters = false;
-				}
-			},
-			getAllProducts() {
-				this.initiateLoadingSearchResults(true);
+export default {
+    data() {
+        return {
+            loader: false,
+            disableFilters: false,
+            model: "products",
+            search: "",
+            headers: [
+                {
+                    text: "Products",
+                    align: "left",
+                    sortable: false,
+                    value: "name"
+                },
+                { text: "Id", value: "id" },
+                { text: "Sku", value: "sku" },
+                { text: "Photo url", value: "photo_url" },
+                { text: "Price", value: "price.amount" }
+            ]
+        };
+    },
+    mounted() {
+        this.getAllProducts();
+    },
+    computed: {
+        productList: {
+            get() {
+                return this.$store.state.productList;
+            },
+            set(value) {
+                this.$store.state.productList = value;
+            }
+        }
+    },
+    methods: {
+        initiateLoadingSearchResults(loading) {
+            if (loading) {
+                this.loader = true;
+                this.disableFilters = true;
+                this.productList = [];
+            } else {
+                this.loader = false;
+                this.disableFilters = false;
+            }
+        },
+        getAllProducts() {
+            this.initiateLoadingSearchResults(true);
 
-				let payload = {
-					model: "products",
-					mutation: "setProductList"
-				};
-				this.$store
-					.dispatch("getAll", payload)
-					.then(result => {
-						this.initiateLoadingSearchResults(false);
-					})
-					.catch(error => {
-						this.initiateLoadingSearchResults(false);
-						console.log(error);
-					});
-			},
-			searchProduct() {
-				if (this.keyword.length > 0) {
-					this.initiateLoadingSearchResults(true);
+            let payload = {
+                model: "products",
+                mutation: "setProductList"
+            };
+            this.$store
+                .dispatch("getAll", payload)
+                .then(result => {
+                    this.initiateLoadingSearchResults(false);
+                })
+                .catch(error => {
+                    this.initiateLoadingSearchResults(false);
+                    console.log(error);
+                });
+        },
+        searchProduct() {
+            if (this.keyword.length > 0) {
+                this.initiateLoadingSearchResults(true);
 
-					let payload = {
-						model: "products",
-						mutation: "setProductList",
-						keyword: this.keyword
-					};
+                let payload = {
+                    model: "products",
+                    mutation: "setProductList",
+                    keyword: this.keyword
+                };
 
-					this.$store
-						.dispatch("search", payload)
-						.then(result => {
-							this.initiateLoadingSearchResults(false);
-						})
-						.catch(error => {
-							this.initiateLoadingSearchResults(false);
-						});
-				}
-			}
-		}
-	};
+                this.$store
+                    .dispatch("search", payload)
+                    .then(result => {
+                        this.initiateLoadingSearchResults(false);
+                    })
+                    .catch(error => {
+                        this.initiateLoadingSearchResults(false);
+                    });
+            }
+        }
+    }
+};
 </script>
