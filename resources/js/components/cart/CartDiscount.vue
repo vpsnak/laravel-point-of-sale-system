a082b69614bfada838789b825c63df53b4630428
Pavlos Kafritsas <pavkfr@hotmail.com>
2019-09-28 01:17:23 +0300

    fixed discount type bug

--- resources/js/components/cart/CartDiscount.vue
+++ resources/js/components/cart/CartDiscount.vue
@@ -1,21 +1,32 @@
 <template>
 	<div class="d-flex">
-		<v-col cols="6" class="pa-0 pr-2">
+		<v-col
+			cols="6"
+			class="pa-0 pr-2"
+		>
 			<v-select
 				v-if="editable"
 				:key="model.id"
-				v-model="type"
+				v-model="discountType"
 				:items="discountTypes"
 				label="Discount"
 				item-text="label"
 				item-value="value"
 			></v-select>
-			<v-text-field v-else-if="!editable" :value="discountType" label="Discount" disabled></v-text-field>
+			<v-text-field
+				v-else-if="!editable"
+				:value="discountType"
+				label="Discount"
+				disabled
+			></v-text-field>
 		</v-col>
-		<v-col cols="6" class="pa-0 pl-2">
+		<v-col
+			cols="6"
+			class="pa-0 pl-2"
+		>
 			<v-text-field
 				:key="model.id"
-				v-if="type && type !== 'None'"
+				v-if="discountType && discountType !== 'None'"
 				v-model="amount"
 				type="number"
 				label="Amount"
@@ -27,38 +38,38 @@
 </template>
 
 <script>
-import { mapState } from "vuex";
-import { mapGetters } from "vuex";
+	import { mapState } from "vuex";
+	import { mapGetters } from "vuex";
 
-export default {
-	props: {
-		model: Array | Object,
-		editable: Boolean
-	},
-	computed: {
-		...mapState("cart", ["discountTypes"]),
-		type: {
-			get() {
-				return this.$props.model.discount_type
-					? this.$props.model.discount_type
-					: "None";
+	export default {
+		props: {
+			model: Array | Object,
+			editable: Boolean
+		},
+		computed: {
+			...mapState("cart", ["discountTypes"]),
+			discountType: {
+				get() {
+					return this.$props.model.discount_type
+						? this.$props.model.discount_type
+						: "None";
+				},
+				set(value) {
+					this.$set(this.$props.model, "discount_type", value);
+				}
 			},
-			set(value) {
-				this.$set(this.$props.model, "discount_type", value);
+			amount: {
+				get() {
+					return this.$props.model.discount_amount;
+				},
+				set(value) {
+					this.$set(this.$props.model, "discount_amount", value);
+					this.$store.commit("cart/setDiscount", this.$props.model);
+				}
 			}
 		},
-		amount: {
-			get() {
-				return this.$props.model.discount_amount;
-			},
-			set(value) {
-				this.$set(this.$props.model, "discount_amount", value);
-				this.$store.commit("cart/setDiscount", this.$props.model);
-			}
+		methods: {
+			setDiscount() {}
 		}
-	},
-	methods: {
-		setDiscount() {}
-	}
-};
+	};
 </script>
