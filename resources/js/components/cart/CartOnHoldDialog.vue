52c5428b3fbbdc52e484e4fc1df5401ec7fb0b0e
Pavlos Kafritsas <pavkfr@hotmail.com>
2019-09-28 10:46:09 +0300

    partially refactored cartOnHold

---
+++ resources/js/components/cart/CartOnHoldDialog.vue
@@ -0,0 +1,16 @@
+<template>
+	<v-dialog
+		v-model="show"
+		width="500"
+	>
+		<restoreCartDialog />
+	</v-dialog>
+</template>
+
+<script>
+	export default {
+		props: {
+			show: Boolean
+		}
+	};
+</script>
