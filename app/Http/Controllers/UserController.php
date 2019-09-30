6db65fe7466af08a85408a40374639437ece3c86
Vpsnak <info.vpsnak@gmail.com>
2019-09-30 22:43:16 +0300

    user api now works without search or create

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,7 +4,23 @@ namespace App\Http\Controllers;
 
 use App\User;
 
-class UserController extends BaseController
+class UserController extends Controller
 {
     protected $model = User::class;
+    
+    public function all()
+    {
+        return response($this->model::all(), 200);
+    }
+    
+    public function get($id)
+    {
+        return response($this->model::find($id), 200);
+    }
+    
+    public function delete($id)
+    {
+        $deleted = $this->model::where('id', $id)->delete();
+        return response($deleted, 200);
+    }
 }
