805eaa5dd3d900dc3f9616599167f65bfef9d957
nikos <nikoskalmah@hotmail.com>
2019-10-01 17:36:54 +0300

    added disable button prop for tables

--- resources/js/components/forms/UserForm.vue
+++ resources/js/components/forms/UserForm.vue
@@ -1,71 +1,71 @@
 <template>
-    <v-form v-model="valid">
-        <div class="text-center">
-            <v-chip color="indigo darken-4" label>
-                <v-icon left>fas fa-user-circle</v-icon>User Form
-            </v-chip>
-        </div>
-        <v-text-field v-model="name" :rules="nameRules" :counter="10" label="Name" required></v-text-field>
-        <v-text-field v-model="email" :rules="emailRules" label="E-mail" required></v-text-field>
-        <v-text-field
-            v-model="password"
-            :append-icon="show ? 'visibility' : 'visibility_off'"
-            :rules="[passwordRules.required, passwordRules.min]"
-            :type="show ? 'text' : 'password'"
-            name="input-10-1"
-            label="Password"
-            hint="At least 8 characters"
-            counter
-            @click:append="show = !show"
-        ></v-text-field>
-        <v-btn class="mr-4" @click="submit">submit</v-btn>
-        <v-btn @click="clear">clear</v-btn>
-    </v-form>
+	<v-form v-model="valid">
+		<div class="text-center">
+			<v-chip color="indigo darken-4" label>
+				<v-icon left>fas fa-user-circle</v-icon>User Form
+			</v-chip>
+		</div>
+		<v-text-field v-model="name" :rules="nameRules" :counter="10" label="Name" required></v-text-field>
+		<v-text-field v-model="email" :rules="emailRules" label="E-mail" required></v-text-field>
+		<v-text-field
+			v-model="password"
+			:append-icon="show ? 'visibility' : 'visibility_off'"
+			:rules="[passwordRules.required, passwordRules.min]"
+			:type="show ? 'text' : 'password'"
+			name="input-10-1"
+			label="Password"
+			hint="At least 8 characters"
+			counter
+			@click:append="show = !show"
+		></v-text-field>
+		<v-btn class="mr-4" @click="submit">submit</v-btn>
+		<v-btn @click="clear">clear</v-btn>
+	</v-form>
 </template>
 
 <script>
-import { mapActions } from "vuex";
+	import { mapActions } from "vuex";
 
-export default {
-    data: () => ({
-        valid: false,
-        name: "",
-        nameRules: [
-            v => !!v || "Name is required",
-            v => v.length <= 10 || "Name must be less than 10 characters"
-        ],
-        email: "",
-        emailRules: [
-            v => !!v || "E-mail is required",
-            v => /.+@.+/.test(v) || "E-mail must be valid"
-        ],
-        show: false,
-        password: "Password",
-        passwordRules: {
-            required: value => !!value || "Required.",
-            min: v => v.length >= 8 || "Min 8 characters"
-        }
-    }),
-    methods: {
-        submit() {
-            let payload = {
-                model: "users",
-                data: {
-                    name: this.name,
-                    email: this.email,
-                    password: this.password
-                }
-            };
-            this.create(payload).then(() => {});
-        },
-        clear() {
-            this.name = "";
-            this.email = "";
-            this.password = "";
-        },
-        ...mapActions({
-            create: "create"
-        })
-    }
-};
+	export default {
+		data: () => ({
+			valid: false,
+			name: "",
+			nameRules: [
+				// v => !!v || "Name is required",
+				v => v.length <= 10 || "Name must be less than 10 characters"
+			],
+			email: "",
+			emailRules: [
+				// v => !!v || "E-mail is required",
+				v => /.+@.+/.test(v) || "E-mail must be valid"
+			],
+			show: false,
+			password: "Password",
+			passwordRules: {
+				required: value => !!value || "Required.",
+				min: v => v.length >= 8 || "Min 8 characters"
+			}
+		}),
+		methods: {
+			submit() {
+				let payload = {
+					model: "users",
+					data: {
+						name: this.name,
+						email: this.email,
+						password: this.password
+					}
+				};
+				this.create(payload).then(() => {});
+			},
+			clear() {
+				this.name = "";
+				this.email = "";
+				this.password = "";
+			},
+			...mapActions({
+				create: "create"
+			})
+		}
+	};
 </script>
--- resources/js/components/tables/OrderTable.vue
+++ resources/js/components/tables/OrderTable.vue
@@ -1,79 +1,76 @@
 <template>
-    <prop-data-table
-        :tableHeaders="headers"
-        data-url="orders"
-        tableTitle="Orders"
-        tableBtnTitle="New Order"
-    />
+	<prop-data-table
+		:tableHeaders="headers"
+		data-url="orders"
+		tableTitle="Orders"
+		tableBtnTitle="New Order"
+		tableBtnDisable="true"
+	/>
 </template>
 
 <script>
-import { mapMutations } from "vuex";
+	import { mapMutations } from "vuex";
 
-export default {
-    data() {
-        return {
-            headers: [
-                {
-                    text: "Customer id",
-                    value: "customer_id"
-                },
-                {
-                    text: "Store id",
-                    value: "store_id"
-                },
-                {
-                    text: "Created_by",
-                    value: "created_by"
-                },
-                {
-                    text: "Items",
-                    value: "items"
-                },
-                {
-                    text: "Discount type",
-                    value: "discount_type"
-                },
-                {
-                    text: "Discount amount",
-                    value: "discount_amount"
-                },
-                {
-                    text: "Shipping type",
-                    value: "shipping_type"
-                },
-                {
-                    text: "Shipping cost",
-                    value: "shipping_cost"
-                },
-                {
-                    text: "tax",
-                    value: "tax"
-                },
-                {
-                    text: "Subtotal",
-                    value: "subtotal"
-                },
-                {
-                    text: "Note",
-                    value: "note"
-                },
-                {
-                    text: "Actions",
-                    value: "action",
-                    sortable: false
-                }
-            ]
-        };
-    },
-    mounted() {
-        this.setRows([]);
-    },
-    methods: {
-        ...mapMutations("datatable", {
-            setRows: "setRows",
-            deleteRow: "deleteRow"
-        })
-    }
-};
+	export default {
+		data() {
+			return {
+				headers: [
+					{
+						text: "Customer id",
+						value: "customer_id"
+					},
+					{
+						text: "Store id",
+						value: "store_id"
+					},
+					{
+						text: "Created_by",
+						value: "created_by"
+					},
+					{
+						text: "Discount type",
+						value: "discount_type"
+					},
+					{
+						text: "Discount amount",
+						value: "discount_amount"
+					},
+					{
+						text: "Shipping type",
+						value: "shipping_type"
+					},
+					{
+						text: "Shipping cost",
+						value: "shipping_cost"
+					},
+					{
+						text: "tax",
+						value: "tax"
+					},
+					{
+						text: "Subtotal",
+						value: "subtotal"
+					},
+					{
+						text: "Note",
+						value: "note"
+					},
+					{
+						text: "Actions",
+						value: "action",
+						sortable: false
+					}
+				]
+			};
+		},
+		mounted() {
+			this.setRows([]);
+		},
+		methods: {
+			...mapMutations("datatable", {
+				setRows: "setRows",
+				deleteRow: "deleteRow"
+			})
+		}
+	};
 </script>
