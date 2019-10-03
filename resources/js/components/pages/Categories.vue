6e56491d254d30d71f3054f634945fc33224fb9b
Nikos Papaioannou <nikoskalmah@hotmail.com>
2019-10-03 22:30:58 +0300

    changes in StoreCashRegDialog

--- resources/js/components/pages/Categories.vue
+++ resources/js/components/pages/Categories.vue
@@ -1,10 +1,9 @@
 <template>
-	<v-container fluid>
-      <v-row>
-         <v-col>
-           <categoryTable>
-           </categoryTable>
-         </v-col>
-      </v-row>
-   </v-container>
+    <v-container fluid>
+        <v-row>
+            <v-col>
+                <categoryTable></categoryTable>
+            </v-col>
+        </v-row>
+    </v-container>
 </template>
