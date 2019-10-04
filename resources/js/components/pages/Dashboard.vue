3b170caaa97db90b07475e7cda201a2c0a85881a
nikos <nikoskalmah@hotmail.com>
2019-10-04 18:16:10 +0300

    fix bug with dialog

--- resources/js/components/pages/Dashboard.vue
+++ resources/js/components/pages/Dashboard.vue
@@ -2,29 +2,35 @@
 	<div>
 		<v-btn @click="fire">Dialog</v-btn>
 
-		<confirmationDialog title="Alvanos" component="paymentActions" @confirmation="confirmation" actions persistent />
+		<interactiveDialog
+			title="Alvanos"
+			component="paymentActions"
+			@confirmation="confirmation"
+			actions
+			persistent
+		/>
 	</div>
 </template>
 
 <script>
-export default {
-	data() {
-		return {
-			show: false
-		};
-	},
-
-	mounted() {
-		this.fire();
-	},
-
-	methods: {
-		confirmation(event) {
-			this.$store.state.confirmationDialog = false;
+	export default {
+		data() {
+			return {
+				show: false
+			};
 		},
-		fire() {
-			this.$store.state.confirmationDialog = true;
+
+		mounted() {
+			this.fire();
+		},
+
+		methods: {
+			confirmation(event) {
+				this.$store.state.interactiveDialog = false;
+			},
+			fire() {
+				this.$store.state.interactiveDialog = true;
+			}
 		}
-	}
-};
+	};
 </script>
