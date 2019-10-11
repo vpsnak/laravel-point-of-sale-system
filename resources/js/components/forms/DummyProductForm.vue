e28e7009c07752b193c98f11d8dbf2d89aae86ff
nikos <nikoskalmah@hotmail.com>
2019-10-11 13:16:11 +0300

    dummy product updated

--- resources/js/components/forms/DummyProductForm.vue
+++ resources/js/components/forms/DummyProductForm.vue
@@ -1,8 +1,8 @@
 <template>
 	<v-form>
-		<v-text-field v-model="formFields.name" counter label="Name" required></v-text-field>
-		<v-text-field v-model="formFields.sku" counter label="Sku" required></v-text-field>
-		<v-text-field type="number" v-model="formFields.final_price" counter label="Final price" required></v-text-field>
+		<v-text-field v-model="formFields.name" label="Name" required></v-text-field>
+		<v-text-field v-model="formFields.sku" label="Sku" required></v-text-field>
+		<v-text-field type="number" v-model="formFields.price.amount" label="Price" required></v-text-field>
 		<v-btn class="mr-4" @click="addProduct(formFields)">Add to cart</v-btn>
 		<v-btn @click="clear">clear</v-btn>
 	</v-form>
@@ -17,22 +17,16 @@ export default {
 	},
 	data() {
 		return {
-			defaultValues: {},
 			formFields: {
 				name: "",
 				sku: null,
-				final_price: null
+				price: {
+					amount: null
+				}
 			}
 		};
 	},
-	mounted() {
-		this.defaultValues = this.formFields;
-		if (this.$props.model) {
-			this.formFields = {
-				...this.$props.model
-			};
-		}
-	},
+
 	methods: {
 		addProduct(product) {
 			this.$store.commit("cart/addProduct", product);
@@ -42,17 +36,17 @@ export default {
 			this.$off("addtocart");
 		},
 		beforeDestroy() {
-			this.$off("submit");
+			this.$off("addtocart");
 		},
 		clear() {
-			this.formFields = { ...this.defaultValues };
-		},
-		...mapActions({
-			getAll: "getAll",
-			getOne: "getOne",
-			create: "create",
-			delete: "delete"
-		})
+			this.formFields = {
+				name: "",
+				sku: null,
+				price: {
+					amount: 0
+				}
+			};
+		}
 	}
 };
 </script>
