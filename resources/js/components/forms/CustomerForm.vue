7500c61f988369e337c4b23f5c05e243c56ab3fb
nikos <nikoskalmah@hotmail.com>
2019-10-26 11:54:15 +0300

    customer form clear fixed

--- resources/js/components/forms/CustomerForm.vue
+++ resources/js/components/forms/CustomerForm.vue
@@ -11,15 +11,15 @@ formFields<template>
 			<v-text-field v-model="formFields.email" label="Email" required></v-text-field>
 			<v-text-field v-model="formFields.phone" label="Phone" required></v-text-field>
 			<v-text-field v-model="formFields.company_name" label="Company name" required></v-text-field>
-			<v-text-field v-model="formFields.addresses.area_code_id" counter label="Area Code id" required></v-text-field>
-			<v-text-field v-model="formFields.addresses.first_name" counter label="First name" required></v-text-field>
-			<v-text-field v-model="formFields.addresses.last_name" counter label="Last name" required></v-text-field>
-			<v-text-field v-model="formFields.addresses.street" counter label="Street" required></v-text-field>
-			<v-text-field v-model="formFields.addresses.city" counter label="City" required></v-text-field>
-			<v-text-field v-model="formFields.addresses.country_id" counter label="Country id" required></v-text-field>
-			<v-text-field v-model="formFields.addresses.region" counter label="Region" required></v-text-field>
-			<v-text-field v-model="formFields.addresses.postcode" counter label="Postcode" required></v-text-field>
-			<v-text-field v-model="formFields.addresses.phone" counter label="Phone" required></v-text-field>
+			<v-text-field v-model="formFields.addresses.area_code_id" label="Area Code id" required></v-text-field>
+			<v-text-field v-model="formFields.addresses.first_name" label="First name" required></v-text-field>
+			<v-text-field v-model="formFields.addresses.last_name" label="Last name" required></v-text-field>
+			<v-text-field v-model="formFields.addresses.street" label="Street" required></v-text-field>
+			<v-text-field v-model="formFields.addresses.city" label="City" required></v-text-field>
+			<v-text-field v-model="formFields.addresses.country_id" label="Country id" required></v-text-field>
+			<v-text-field v-model="formFields.addresses.region" label="Region" required></v-text-field>
+			<v-text-field v-model="formFields.addresses.postcode" label="Postcode" required></v-text-field>
+			<v-text-field v-model="formFields.addresses.phone" label="Phone" required></v-text-field>
 
 			<v-btn class="mr-4" @click="submit">submit</v-btn>
 			<v-btn @click="clear">clear</v-btn>
@@ -30,13 +30,11 @@ formFields<template>
 import { mapActions, mapState, mapGetters } from "vuex";
 
 export default {
-	props: {
-		model: Object || undefined
-	},
+	// props: {
+	// 	model: Object || undefined
+	// },
 	data() {
 		return {
-			addressClear: false,
-			savingSuccessful: false,
 			defaultValues: {},
 			formFields: {
 				first_name: null,
@@ -46,10 +44,10 @@ export default {
 				phone: null,
 				addresses: {
 					area_code_id: null,
-					last_name: null,
-					first_name: null,
-					street: null,
-					city: null,
+					last_name: "",
+					first_name: "",
+					street: "",
+					city: "",
 					country_id: null,
 					region: null,
 					postcode: null,
@@ -58,14 +56,14 @@ export default {
 			}
 		};
 	},
-	mounted() {
-		this.defaultValues = this.formFields;
-		if (this.$props.model) {
-			this.formFields = {
-				...this.$props.model
-			};
-		}
-	},
+	// mounted() {
+	// 	this.defaultValues = this.formFields;
+	// 	if (this.$props.model) {
+	// 		this.formFields = {
+	// 			...this.$props.model
+	// 		};
+	// 	}
+	// },
 	methods: {
 		submit() {
 			let payload = {
@@ -78,8 +76,21 @@ export default {
 			});
 		},
 		clear() {
-			console.log(this.formFields);
-			this.formFields = { ...this.defaultValues };
+			// this.formFields = { ...this.defaultValues };
+			this.formFields.first_name = null;
+			this.formFields.last_name = null;
+			this.formFields.email = null;
+			this.formFields.phone = null;
+			this.formFields.company_name = null;
+			this.formFields.addresses.area_code_id = null;
+			this.formFields.addresses.first_name = null;
+			this.formFields.addresses.last_name = null;
+			this.formFields.addresses.street = null;
+			this.formFields.addresses.city = null;
+			this.formFields.addresses.country_id = null;
+			this.formFields.addresses.region = null;
+			this.formFields.addresses.postcode = null;
+			this.formFields.addresses.phone = null;
 		},
 		beforeDestroy() {
 			this.$off("submit");
