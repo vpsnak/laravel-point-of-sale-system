7531d410e28cc86a8732500017e936dea361dd4f
vpsnak <info.vpsnak@gmail.com>
2019-10-30 16:33:56 +0200

    production seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -17,13 +17,13 @@ class DatabaseSeeder extends Seeder
         $this->call(CashRegisterSeeder::class);
         $this->call(PaymentType::class);
         // $this->call(ShippingSeeder::class);
-        $this->call(CustomerSeeder::class);
-        $this->call(ProductSeeder::class);
-        $this->call(CategorySeeder::class);
+//        $this->call(CustomerSeeder::class);
+//        $this->call(ProductSeeder::class);
+//        $this->call(CategorySeeder::class);
 
         $this->call(CouponSeeder::class);
         $this->call(GiftcardSeeder::class);
- 
+
         // $this->call(OrderSeeder::class);
         // $this->call(PaymentSeeder::class);
 
