5c21d60671d818117df1d1f1282902b32c74383f
vpsnak <info.vpsnak@gmail.com>
2019-11-02 12:05:49 +0200

    customer controller now searching for address too

--- app/Http/Controllers/CustomerController.php
+++ app/Http/Controllers/CustomerController.php
@@ -12,23 +12,51 @@ class CustomerController extends BaseController
     public function create(Request $request)
     {
         $validatedData = $request->validate([
-            'customer_id' => 'sometimes|exists:customers,id',
             'email' => 'required|email|unique:customers,email',
             'first_name' => 'required|string',
             'last_name' => 'required|string',
         ]);
 
-        if (array_key_exists('customer_id', $validatedData)) {
-            $customer = $this->model::find($validatedData['customer_id']);
-            unset($validatedData['customer_id']);
-        } else {
-            $customer = new Customer($validatedData);
-        }
+        $validatedExtra = $request->validate([
+            'id' => 'nullable|exists:customers,id',
+            'address' => 'nullable|array',
+        ]);
 
-        $customer->save();
+        $customer = $this->getCustomer($validatedExtra['id'] ?? null, $validatedData);
+
+        if (!empty($validatedExtra['address'])) {
+            $addressData = $request->validate([
+                'address.first_name' => 'required|string',
+                'address.last_name' => 'required|string',
+                'address.street' => 'required|string',
+                'address.street2' => 'nullable|string',
+                'address.city' => 'required|string',
+                'address.country_id' => 'required|exists:countries,country_id',
+                'address.region' => 'required|exists:regions,region_id',
+                'address.postcode' => 'required|string',
+                'address.phone' => 'required|numeric',
+                'address.company' => 'nullable|string',
+                'address.vat_id' => 'nullable|string',
+                'address.billing' => 'nullable|bool',
+                'address.shipping' => 'nullable|bool',
+            ]);
+
+            $customer->addresses()->create($addressData['address']);
+        }
 
         // @TODO fix update
         return response($customer, 201);
+    }
+
+    private function getCustomer($id, $data)
+    {
+        if (!empty($id)) {
+            $this->model::updateData($id, $data);
+            $model = $this->model::getOne($id);
+        } else {
+            $model = $this->model::store($data);
+        }
+        return $model;
     }
 
     public function search(Request $request)
