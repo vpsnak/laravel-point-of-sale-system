d12110cc22dbc764e90b0fafb4b671f027385bc4
vpsnak <info.vpsnak@gmail.com>
2019-11-02 12:53:45 +0200

    added extra fields in controller and fixed validation for updating user email while checking it is unique

--- app/Customer.php
+++ app/Customer.php
@@ -8,7 +8,13 @@ class Customer extends BaseModel
         'magento_id',
         'first_name',
         'last_name',
-        'email'
+        'email',
+        'house_account_status',
+        'house_account_number',
+        'house_account_limit',
+        'no_tax',
+        'no_tax_file',
+        'comment',
     ];
 
     protected $with = ['addresses'];
--- app/Http/Controllers/CustomerController.php
+++ app/Http/Controllers/CustomerController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Customer;
 use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
 
 class CustomerController extends BaseController
 {
@@ -11,16 +12,40 @@ class CustomerController extends BaseController
 
     public function create(Request $request)
     {
-        $validatedData = $request->validate([
-            'email' => 'required|email|unique:customers,email',
-            'first_name' => 'required|string',
-            'last_name' => 'required|string',
-        ]);
-
         $validatedExtra = $request->validate([
             'id' => 'nullable|exists:customers,id',
             'address' => 'nullable|array',
         ]);
+
+        if (!empty($validatedExtra['id'])) {
+            $validatedData = $request->validate([
+                'email' => [
+                    'required',
+                    'email',
+                    Rule::unique('customers', 'email')->ignore($validatedExtra['id'])
+                ],
+                'first_name' => 'required|string',
+                'last_name' => 'required|string',
+                'house_account_status' => 'nullable|bool',
+                'house_account_number' => 'nullable|string',
+                'house_account_limit' => 'nullable|numeric',
+                'no_tax' => 'nullable|bool',
+                'no_tax_file' => 'nullable|string',
+                'comment' => 'nullable|text',
+            ]);
+        } else {
+            $validatedData = $request->validate([
+                'email' => 'required|email|unique:customers,email',
+                'first_name' => 'required|string',
+                'last_name' => 'required|string',
+                'house_account_status' => 'nullable|bool',
+                'house_account_number' => 'nullable|string',
+                'house_account_limit' => 'nullable|numeric',
+                'no_tax' => 'nullable|bool',
+                'no_tax_file' => 'nullable|string',
+                'comment' => 'nullable|text',
+            ]);
+        }
 
         $customer = $this->getCustomer($validatedExtra['id'] ?? null, $validatedData);
 
