2d357afb8423304008f21ca227d458a7d1b7c04d
nikos <nikoskalmah@hotmail.com>
2019-11-04 16:39:28 +0200

    bug with open cash register fixed

--- resources/js/components/menu/TopMenu.vue
+++ resources/js/components/menu/TopMenu.vue
@@ -32,7 +32,7 @@
 				<v-btn v-on="on" icon>
 					<v-icon>mdi-account-circle</v-icon>
 				</v-btn>
-				<v-btn v-if="!openedRegister" text @click="OpenCashRegisterDialog = true">Open cash register</v-btn>
+				<v-chip v-if="!openedRegister" text @click="OpenCashRegisterDialog = true">Open cash register</v-chip>
 				<v-chip
 					v-else
 					@click="CloseCashRegisterDialog = true"
