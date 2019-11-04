2e177e7cbad584465aab118b1e2a0d0f73274719
nikos <nikoskalmah@hotmail.com>
2019-11-04 17:50:06 +0200

    open cash register page

---
+++ resources/js/components/pages/OpenCashRegister.vue
@@ -0,0 +1,11 @@
+<template>
+	<v-container class="fill-height" fluid>
+		<v-row align="center" justify="center">
+			<v-col cols="12" sm="8" md="4">
+				<v-card class="elevation-12">
+					<openCashRegisterForm></openCashRegisterForm>
+				</v-card>
+			</v-col>
+		</v-row>
+	</v-container>
+</template>
