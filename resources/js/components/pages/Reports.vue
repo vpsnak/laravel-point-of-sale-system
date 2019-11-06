9a3c224d98fe1d50d8f6540f6da12ee5c0faaa72
nikos <nikoskalmah@hotmail.com>
2019-11-06 16:18:33 +0200

    remove tabs add slide buttons to render correctly the data tables

--- resources/js/components/pages/Reports.vue
+++ resources/js/components/pages/Reports.vue
@@ -2,33 +2,34 @@
 	<v-container fluid>
 		<v-row>
 			<v-col>
-				<v-card>
-					<v-card-title class="text-center justify-center py-6">
-						<h1 class="font-weight-bold display-3 primary--text">Reports</h1>
-					</v-card-title>
-
-					<v-tabs v-model="tabs" background-color="transparent" grow>
-						<v-tab>Cash Register</v-tab>
-						<v-tab>User</v-tab>
-					</v-tabs>
-					<v-tabs-items v-model="tabs">
-						<v-tab-item>
-							<v-card flat>
-								<v-card-text>
-									lalalalla
-									<cashRegisterReportsTable></cashRegisterReportsTable>
-								</v-card-text>
-							</v-card>
-						</v-tab-item>
-						<v-tab-item>
-							<v-card flat>
-								<v-card-text>
-									<userTable></userTable>
-								</v-card-text>
-							</v-card>
-						</v-tab-item>
-					</v-tabs-items>
-				</v-card>
+				<v-slide-group show-arrows v-model="selectedTable">
+					<v-slide-item v-slot:default="{ active, toggle }">
+						<v-btn
+							v-model="cashRegister"
+							:color="active ? 'blue-grey' : ''"
+							class="mx-1"
+							@click="toggle, cashRegister = true, user = false;"
+							depressed
+							rounded
+						>Cash Register</v-btn>
+					</v-slide-item>
+					<v-slide-item v-slot:default="{ active, toggle }">
+						<v-btn
+							v-model="user"
+							:color="active ? 'blue-grey' : ''"
+							class="mx-1"
+							@click="toggle, cashRegister = false, user = true;"
+							depressed
+							rounded
+						>User</v-btn>
+					</v-slide-item>
+				</v-slide-group>
+				<div class="mt-2" v-if="cashRegister">
+					<cashRegisterReportsTable></cashRegisterReportsTable>
+				</div>
+				<div class="mt-2" v-if="user">
+					<userTable></userTable>
+				</div>
 			</v-col>
 		</v-row>
 	</v-container>
@@ -38,7 +39,9 @@
 export default {
 	data() {
 		return {
-			tabs: null
+			selectedTable: null,
+			cashRegister: false,
+			user: false
 		};
 	}
 };
