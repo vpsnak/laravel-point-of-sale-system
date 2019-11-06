153c5dd6b0a1c1ac3cea6eac3f359c31ba0d8433
nikos <nikoskalmah@hotmail.com>
2019-11-06 11:43:56 +0200

    add product in cart when is scanned by search bar

--- resources/js/components/lists/ProductList.vue
+++ resources/js/components/lists/ProductList.vue
@@ -67,7 +67,12 @@
 			</v-row>
 			<v-row v-if="productList.length" style="height:61vh; overflow-y:auto;">
 				<v-col v-for="product in productList" :key="product.id" cols="12" md="6" lg="4">
-					<v-card :img="product.photo_url" @click="addProduct(product)" height="170px">
+					<v-card
+						v-model="current_product"
+						:img="product.photo_url"
+						@click="addProduct(product)"
+						height="170px"
+					>
 						<v-card-title class="blue-grey pa-0" @click.stop>
 							<h6 class="px-2">{{product.name}}</h6>
 							<div class="flex-grow-1"></div>
@@ -155,9 +160,11 @@
 </template>
 
 <script>
+import { log } from "util";
 export default {
 	data() {
 		return {
+			current_product: null,
 			current_page: 1,
 			last_page: null,
 			showViewDialog: false,
@@ -339,6 +346,14 @@ export default {
 						this.lastPage = response.last_page;
 					})
 					.finally(() => {
+						if (
+							this.productList.length === 1 &&
+							this.btnactive == true
+						) {
+							for (const product of this.productList) {
+								this.addProduct(product);
+							}
+						}
 						this.initiateLoadingSearchResults(false);
 					});
 			} else {
