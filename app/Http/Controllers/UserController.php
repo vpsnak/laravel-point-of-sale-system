416dbf3c80f986b5dee21204bc8d31af81249f2b
Pavlos Kafritsas <pavkfr@hotmail.com>
2019-11-07 13:08:33 +0200

    grant token w/ role name @ user login

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -34,29 +34,38 @@ class UserController extends Controller
         ]);
 
         $http = new Client;
+        $user = User::whereEmail($validatedData['username'])->first();
+        if (!$user) {
+            return response([
+                'notification' => [
+                    'msg' => '<h2>Invalid credentials</h2>',
+                    'type' => 'error'
+                ]
+            ], 401);
+        } else {
+            $role = ($user->roles)[0]['name'];
+            $response = $http->post(config('app.url') . '/oauth/token', [
+                'form_params' => [
+                    'grant_type' => 'password',
+                    'client_id' => '2',
+                    'client_secret' => 'wtx0OMH1wmzMjVu8KV72vC6QXDqijRrBJygHJRV7',
+                    'username' => $validatedData['username'],
+                    'password' => $validatedData['password'],
+                    'scope' => $role,
+                ],
+            ]);
 
-        $response = $http->post(config('app.url') . '/oauth/token', [
-            'form_params' => [
-                'grant_type' => 'password',
-                'client_id' => '2',
-                'client_secret' => 'wtx0OMH1wmzMjVu8KV72vC6QXDqijRrBJygHJRV7',
-                'username' => $validatedData['username'],
-                'password' => $validatedData['password'],
-                'scope' => '',
-            ],
-        ]);
+            $token = (json_decode((string) $response->getBody(), true))['access_token'];
 
-        $user = User::whereEmail($validatedData['username'])->firstOrFail();
-        $token = (json_decode((string) $response->getBody(), true))['access_token'];
-
-        return response([
-            'notification' => [
-                'msg' => '<h2>Welcome ' . $user->name . '</h2>',
-                'type' => 'info'
-            ],
-            'user' => $user,
-            'token' => $token
-        ], 200);
+            return response([
+                'notification' => [
+                    'msg' => '<h2>Welcome ' . $user->name . '</h2>',
+                    'type' => 'info'
+                ],
+                'user' => $user,
+                'token' => $token
+            ], 200);
+        }
     }
 
     public function logout()
