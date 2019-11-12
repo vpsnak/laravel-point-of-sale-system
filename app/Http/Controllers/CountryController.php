0f6520d1fdccdef3260f4b07079c58a22a44a74e
Pavlos Kafritsas <pavkfr@hotmail.com>
2019-11-12 15:45:29 +0200

    fixed pagination issue @ countries and regions

--- app/Http/Controllers/CountryController.php
+++ app/Http/Controllers/CountryController.php
@@ -8,4 +8,9 @@ use Illuminate\Http\Request;
 class CountryController extends BaseController
 {
     protected $model = Country::class;
-} 
+
+    public function all()
+    {
+        return response(Country::all());
+    }
+}
