b7e89462e66efa262a13faba3a3bbdcb492286ac
vpsnak <info.vpsnak@gmail.com>
2019-11-13 18:45:01 +0200

    giftcard bug on production solved

--- routes/api.php
+++ routes/api.php
@@ -346,7 +346,7 @@ $allRoutes = [
         ]
     ],
     'gift-cards' => [
-        'controller' => 'GiftCardController',
+        'controller' => 'GiftcardController',
         'endpoints' => [
             'get' => [
                 $routeAll,
