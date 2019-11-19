061a6de2b4596ed30bcbcc52a2242e0148901a7b
Pavlos Kafritsas <pavkfr@hotmail.com>
2019-11-19 13:24:22 +0200

    seeder reordering

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,27 +11,28 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        $this->call(OauthClientsSeeder::class);
         $this->call(RoleSeeder::class);
         $this->call(UserSeeder::class);
+
         $this->call(TaxSeeder::class);
         $this->call(StoreSeeder::class);
         $this->call(CashRegisterSeeder::class);
         $this->call(PaymentType::class);
+        $this->call(StorePickupSeeder::class);
+
         // $this->call(ShippingSeeder::class);
         $this->call(CustomerSeeder::class);
         $this->call(ProductSeeder::class);
         $this->call(CategorySeeder::class);
 
-        $this->call(CouponSeeder::class);
-        $this->call(GiftcardSeeder::class);
-
-        $this->call(StorePickupSeeder::class);
-
         // $this->call(OrderSeeder::class);
         // $this->call(PaymentSeeder::class);
 
-        $this->call(OauthClientsSeeder::class);
         $this->call(PlantshedPostCodes::class);
         $this->call(PlantshedAreasSeeder::class);
+
+        $this->call(CouponSeeder::class);
+        $this->call(GiftcardSeeder::class);
     }
 }
