093f3b6998e58ce5c35b2aa64affae5e3fb6323a
Pavlos Kafritsas <pkafritsas@webo2.gr>
2020-01-02 14:11:21 +0200

    fixed bug @ timestamps

--- app/Models/Shipping/Location.php
+++ app/Models/Shipping/Location.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Location extends Model
 {
-    protected $timestamps = false;
+    public $timestamps = false;
 
     protected $fillable = ['name'];
 }
