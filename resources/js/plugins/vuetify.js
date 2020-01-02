aa1b550afab8fa6ba079ef8f57a33b770fe6f888
Pavlos Kafritsas <pkafritsas@webo2.gr>
2020-01-02 13:48:34 +0200

    fixed typo

--- resources/js/plugins/vuetify.js
+++ resources/js/plugins/vuetify.js
@@ -1,10 +1,12 @@
 import Vue from "vue";
 import Vuetify from "vuetify";
 import "vuetify/dist/vuetify.min.css";
+import { preset } from "vue-cli-plugin-vuetify-preset-reply/preset";
 
 Vue.use(Vuetify);
 
-const opts = {
+const options = {
+    breakpoint: { scrollbarWidth: 12 },
     theme: {
         dark: true
     },
@@ -13,4 +15,4 @@ const opts = {
     }
 };
 
-export default new Vuetify(opts);
+export default new Vuetify({ preset, ...options });
