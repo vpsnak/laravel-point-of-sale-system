a0867542e94ce1298722d0a8f4e99026d91f5939
nikos <nikoskalmah@hotmail.com>
2020-01-20 19:30:54 +0200

    fix bug with dialogs

--- app/Http/Controllers/Payment/ElavonApiPaymentController.php
+++ app/Http/Controllers/Payment/ElavonApiPaymentController.php
@@ -79,7 +79,6 @@ class ElavonApiPaymentController extends Controller
         ];
 
         $payload = array_merge($defaults, $data);
-        var_dump($data);
         $html = self::generateHtmlPayload($payload);
         $response = self::sendTransaction($html);
         return (array) $response;
--- resources/js/components/blocks/InteractiveDialog.vue
+++ resources/js/components/blocks/InteractiveDialog.vue
@@ -1,146 +1,142 @@
 <template>
-    <v-dialog
-        v-model="visibility"
-        :persistent="persistent"
-        :max-width="maxWidth"
-        :fullscreen="fullscreen"
-        scrollable
-    >
-        <v-card>
-            <v-card-title>
-                <v-icon v-if="icon" class="pr-2">{{ icon }}</v-icon>
-                {{ title }}
-                <v-spacer v-if="titleCloseBtn"></v-spacer>
-                <v-btn
-                    v-if="titleCloseBtn"
-                    @click.stop="closeEvent(false)"
-                    icon
-                >
-                    <v-icon>mdi-close</v-icon>
-                </v-btn>
-            </v-card-title>
+	<v-dialog
+		v-model="visibility"
+		:persistent="persistent"
+		:max-width="maxWidth"
+		:fullscreen="fullscreen"
+		scrollable
+		@click:outside.stop="closeEvent(false)"
+	>
+		<v-card>
+			<v-card-title>
+				<v-icon v-if="icon" class="pr-2">{{ icon }}</v-icon>
+				{{ title }}
+				<v-spacer v-if="titleCloseBtn"></v-spacer>
+				<v-btn v-if="titleCloseBtn" @click.stop="closeEvent(false, true)" icon>
+					<v-icon>mdi-close</v-icon>
+				</v-btn>
+			</v-card-title>
 
-            <v-divider class="mb-3"></v-divider>
+			<v-divider class="mb-3"></v-divider>
 
-            <v-card-text>
-                <component
-                    :is="component"
-                    v-if="component"
-                    :model="model"
-                    :readOnly="readOnly"
-                    @submit="submit"
-                ></component>
-                <div v-else v-html="content" :class="contentClass || ''"></div>
-            </v-card-text>
+			<v-card-text>
+				<component
+					:is="component"
+					v-if="component"
+					:model="model"
+					:readOnly="readOnly"
+					@submit="submit"
+				></component>
+				<div v-else v-html="content" :class="contentClass || ''"></div>
+			</v-card-text>
 
-            <!-- confirmation actions -->
-            <v-card-actions
-                class="d-flex align-center mt-5"
-                v-if="action === 'confirmation' || action === 'info'"
-            >
-                <div class="flex-grow-1"></div>
+			<!-- confirmation actions -->
+			<v-card-actions
+				class="d-flex align-center mt-5"
+				v-if="action === 'confirmation' || action === 'info'"
+			>
+				<div class="flex-grow-1"></div>
 
-                <v-btn
-                    v-if="action === 'confirmation'"
-                    @click="closeEvent(false)"
-                    text
-                    color="error"
-                    >{{ cancelBtn }}</v-btn
-                >
+				<v-btn
+					v-if="action === 'confirmation'"
+					@click="closeEvent(false, true)"
+					text
+					color="error"
+				>{{ cancelBtn }}</v-btn>
 
-                <v-btn @click="closeEvent(true)" text color="success">{{
-                    confirmationBtn
-                }}</v-btn>
-            </v-card-actions>
-        </v-card>
-    </v-dialog>
+				<v-btn @click="closeEvent(true, true)" text color="success">{{ confirmationBtn }}</v-btn>
+			</v-card-actions>
+		</v-card>
+	</v-dialog>
 </template>
 
 <script>
 import { mapActions, mapMutations, mapState } from "vuex";
 
 export default {
-    props: {
-        show: Boolean,
-        persistent: Boolean,
-        width: Number,
-        icon: String,
-        title: String,
-        titleCloseBtn: Boolean,
+	props: {
+		show: Boolean,
+		persistent: Boolean,
+		width: Number,
+		icon: String,
+		title: String,
+		titleCloseBtn: Boolean,
 
-        content: String,
-        contentClass: String,
-        component: String,
-        fullscreen: Boolean,
-        cancelBtnTxt: String,
-        confirmationBtnTxt: String,
+		content: String,
+		contentClass: String,
+		component: String,
+		fullscreen: Boolean,
+		cancelBtnTxt: String,
+		confirmationBtnTxt: String,
 
-        model: Object,
-        action: String
-    },
+		model: Object,
+		action: String
+	},
 
-    data() {
-        return {
-            display: false
-        };
-    },
+	data() {
+		return {
+			display: false
+		};
+	},
 
-    mounted() {
-        this.display = this.$props.show;
-    },
+	mounted() {
+		this.display = this.$props.show;
+	},
 
-    computed: {
-        visibility: {
-            get() {
-                return this.display;
-            },
-            set(value) {
-                this.display = value;
-            }
-        },
-        maxWidth() {
-            return this.$props.width || 450;
-        },
-        cancelBtn() {
-            return this.$props.cancelBtnTxt || "Cancel";
-        },
-        confirmationBtn() {
-            return this.$props.confirmationBtnTxt || "OK";
-        },
-        readOnly() {
-            return this.$props.action === "read" ? true : false;
-        }
-    },
+	computed: {
+		visibility: {
+			get() {
+				return this.display;
+			},
+			set(value) {
+				this.display = value;
+			}
+		},
+		maxWidth() {
+			return this.$props.width || 450;
+		},
+		cancelBtn() {
+			return this.$props.cancelBtnTxt || "Cancel";
+		},
+		confirmationBtn() {
+			return this.$props.confirmationBtnTxt || "OK";
+		},
+		readOnly() {
+			return this.$props.action === "read" ? true : false;
+		}
+	},
 
-    methods: {
-        ...mapActions(["getAll"]),
-        submit(payload) {
-            if (!payload) {
-                this.closeEvent(true);
-            } else {
-                if (payload.notification) {
-                    this.$store.commit("setNotification", {
-                        msg: payload.notification.msg,
-                        type: payload.notification.type
-                    });
-                }
+	methods: {
+		...mapActions(["getAll"]),
+		submit(payload) {
+			if (!payload) {
+				this.closeEvent(true);
+			} else {
+				if (payload.notification) {
+					this.$store.commit("setNotification", {
+						msg: payload.notification.msg,
+						type: payload.notification.type
+					});
+				}
 
-                if (payload.data) {
-                    this.closeEvent(payload.data);
-                } else {
-                    this.closeEvent(true);
-                }
-            }
-        },
+				if (payload.data) {
+					this.closeEvent(payload.data);
+				} else {
+					this.closeEvent(true);
+				}
+			}
+		},
 
-        closeEvent(payload) {
-            this.$emit("action", payload);
-            this.visibility = false;
-        }
-    },
+		closeEvent(payload, close) {
+			if (!this.$props.persistent || close) {
+				this.$emit("action", payload);
+				this.visibility = false;
+			}
+		}
+	},
 
-    beforeDestroy() {
-        this.$off("action");
-    }
+	beforeDestroy() {
+		this.$off("action");
+	}
 };
 </script>
