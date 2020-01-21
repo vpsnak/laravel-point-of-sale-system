49bc386b20527d1796b327da6b3bdf2e862658d4
nikos <nikoskalmah@hotmail.com>
2020-01-21 13:45:24 +0200

    test blade for receipt

---
+++ resources/views/receipt.blade.php
@@ -0,0 +1,220 @@
+<!DOCTYPE html>
+<html lang="en">
+​
+<head>
+    <meta charset="UTF-8">
+    {{-- <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"> --}}
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+{{-- <style>
+    @font-face {
+        font-family: 'Times New Roman';
+        font-style: normal;
+        font-weight: normal;
+        src: url(http://allfont.net/allfont.css?fonts=courier);
+    }
+</style> --}}
+​
+<body>
+    <div class="receipt" style=" background-color: white; width:auto; height:auto; font-weight:normal; ">
+        <span style="font-size:14px;"> CO-1 PLANTSHED NY FLOWERS </span>
+        <p class="phone" style="text-align: center; font-size:13px;">(212)662-4400</p>
+        <table style="font-size:12px; display:inline-table; width:100%;">
+            <th scope="row" style="text-transform:uppercase; font-weight:normal; float:left;">street
+                98th-st
+            </th>
+            <th scope="row" style="text-transform:uppercase; font-weight:normal; float:right;">new york
+                12345</th>
+        </table>
+        <div class="barcode" style="width:80%;">-------------------------------------------------</div>
+​
+        <table style="font-size:14px; display: inline-table; width:50%; float:left;">
+            <tr>
+                <th scope="row" style="font-weight:normal; text-align: end;">shop:</th>
+                <td style="text-align:start;">R1 posia</td>
+            </tr>
+            <tr>
+                <th scope="row" style="font-weight:normal; text-align: end;">Clrk:</th>
+                <td style="text-align:start;">VANESSA</td>
+            </tr>
+            <tr>
+                <th scope="row" style="font-weight:normal; text-align: end;">Oper:</th>
+                <td style="text-align:start;">VANESSA</td>
+            </tr>
+            <table style="font-size:14px; display: inline-table; width:50%; float:right;">
+​
+                <tr>
+                    <th scope="row" style="font-weight:normal; text-align: end;">Trans:</th>
+                    <td style="text-align:start;">1566551</td>
+                </tr>
+                <tr>
+                    <th scope="row" style="font-weight:normal; text-align: end;">Date:</th>
+                    <td style="text-align:start;">01/02/2019</td>
+                </tr>
+                <tr>
+                    <th scope="row" style="font-weight:normal; text-align: end;">Time:</th>
+                    <td style="text-align:start;">13-12-12</td>
+                </tr>
+            </table>
+        </table>
+        <div class="barcode" style="width:80%;">-------------------------------------------------</div>
+        <table class="products" style="font-size:14px; display: inline-table; width: 100%;">
+            <tr>
+                <th scope="row" style="font-weight:normal; text-transform:uppercase; text-align: start; ">
+                    black roses
+                </th>
+                <td style="text-align: end;">qty(2)</td>
+                <td style="text-align: end;">$1.00</td>
+                <td style="text-align:end;">$2.00</td>
+            </tr>
+            <tr>
+                <td scope="row" style="font-weight:normal; text-align:start; ">Notes
+                    keep it fresh toy bastards
+                </td>
+            </tr>
+            <tr>
+                <th scope="row" style="font-weight:normal; text-transform:uppercase; text-align: start; ">
+                    red roses
+                </th>
+                <td style="text-align: end;">qty(2)</td>
+                <td style="text-align: end;">$1.00</td>
+                <td style="text-align:end;">$2.00</td>
+            </tr>
+            <tr>
+                <th scope="row" style="font-weight:normal; text-align:start; ">pro discount
+                </th>
+                <td></td>
+                <td></td>
+                <td style="text-align:start;">$.50-</td>
+            </tr>
+            <tr>
+                <td scope="row" style="font-weight:normal; text-align:start; ">Notes
+                    keep it fresh toy bastards
+                </td>
+            </tr>
+            <tr>
+                <th scope="row" style="font-weight:normal; text-transform:uppercase; text-align: start; ">
+                    trees with trees
+                </th>
+                <td style="text-align: end;">qty(2)</td>
+                <td style="text-align: end;">$1.00</td>
+                <td style="text-align:end;">$2.00</td>
+            </tr>
+        </table>
+        <table style="font-size:14px; width:100%; ">
+            <tr>
+                <th scope="row" style="font-weight:normal; text-align: end;">Sales tax:</th>
+                <td style="text-align: center;"> $.50</td>
+            </tr>
+            <tr>
+                <th scope="row" style="font-weight:normal; text-align: end;">Total Ant:</th>
+                <td style="text-align: end;">$1.50</td>
+            </tr>
+        </table>
+        <p style="text-align: center; font-size:14px;">*** Tandering detaile ***</p>
+        <table style="font-size:14px; width:100%;">
+            <tr>
+                <th scope="row" style="font-weight:normal; text-align: end;">Order Total is:</th>
+                <td style="text-align: center;">$1.50</td>
+            </tr>
+        </table>
+        <table style="font-size:14px; width:100%;">
+            <tr>
+​
+                <th scope="row" style="font-weight:normal; text-align: start;">01/02/2019</th>
+                <th scope="row" style="font-weight:normal; text-align: start;">Payment Cash</th>
+                <td style="text-align: end;">$1.50-</td>
+            </tr>
+        </table>
+        <table style="font-size:14px; width:100%;">
+            <tr>
+                <th scope="row" style="font-weight:normal; text-align: start;">01/02/2019</th>
+                <th scope="row" style="font-weight:normal; text-align: start;">Gift Card</th>
+                <td style="text-align: end;">$1.50-</td>
+            </tr>
+        </table>
+        <table style="font-size:14px; width:100%;">
+            <tr>
+                <th scope="row" style="font-weight:normal; text-align: start;">01/02/2019</th>
+                <th scope="row" style="font-weight:normal; text-align: start;">Credit Card 0000</th>
+                <td style="text-align: end;">$1.50-</td>
+            </tr>
+        </table>
+        <table style="font-size:14px; width:100%;">
+            <tr>
+                <th scope="row" style="font-weight:normal; text-align: start;">01/02/2019</th>
+                <th scope="row" style="font-weight:normal; text-align: start;">Coupon 0000</th>
+                <td style="text-align: end;">$1.50-</td>
+            </tr>
+        </table>
+        <table style="font-size:14px; width:100%;"><br>
+            <tr>
+                <th scope="row" style="font-weight:normal; text-align: end;">Total Amt Tendered:</th>
+                <td style="text-align: end;">$.00</td>
+            </tr>
+            <tr>
+                <th scope="row" style="font-weight:normal; text-align: end;">Customer Charge:</th>
+                <td style="text-align: end;">$.00</td>
+            </tr>
+        </table>
+        <table style="font-size:14px; width:100%;"><br>
+            <tr>
+                <th scope="row" style="font-weight:normal; text-align: center;">*** Balance Remaining ***</th>
+                <td style="text-align: end;">$.00</td>
+            </tr>
+        </table>
+        <table style="font-size:14px; width:100%;"><br>
+            <tr>
+                <th scope="row" style="font-weight:normal; text-align: end;">Cardholder Name:</th>
+                <td style="text-align: start;">Niggatron</td>
+            </tr>
+            <tr>
+                <th scope="row" style="font-weight:normal; text-align: end;">Credit Cd No:</th>
+                <td style="text-align: start;">1234 vi</td>
+            </tr>
+            <tr>
+                <th scope="row" style="font-weight:normal; text-align: end;">Telephone No:</th>
+                <td style="text-align: start;">21052510235</td>
+            </tr>
+            <tr>
+                <th scope="row" style="font-weight:normal; text-align: end;">Credit Card Tot:</th>
+                <td style="text-align: center;">$1.50</td>
+            </tr>
+        </table>
+        <p style="text-align: center;">------ Credit Card Information ------</p>
+        <table style="font-size:14px; width:100%;">
+            <tr>
+                <th scope="row" style="font-weight:normal; text-align: end;">Delv on:</th>
+                <td style="text-align: start;">12/02/2019 Tue</td>
+            </tr>
+            <tr>
+                <th scope="row" style="font-weight:normal; text-align: end;">Delv to:</th>
+                <td style="text-align: start;">Niggatron</td>
+            </tr>
+            <tr>
+                <th scope="row" style="font-weight:normal; text-align: end;">C/O:</th>
+                <td style="text-align: start;">White Trash</td>
+            </tr>
+            <tr>
+                <th scope="row" style="font-weight:normal; text-align: end;">Address:</th>
+                <td style="text-align: start;">Amarilidos 4</td>
+            </tr>
+            <tr>
+                <th scope="row" style="font-weight:normal; text-align: end;">Address:</th>
+                <td style="text-align: start;">Xalandri</td>
+            </tr>
+            <tr>
+                <th scope="row" style="font-weight:normal; text-align: end;">City, St:</th>
+                <td style="text-align: start;">Athinara</td>
+            </tr>
+        </table>
+        <p style="text-align:end;">(CUstomer's Copy)</p>
+        <span>Signature:___________________</span>
+        <p style="text-align: end;">(Merchant's Copy)</p>
+        <p style="text-align: center;">PLEASE SEE POSTED POLICY REGARDING REFUNDS</p>
+    </div>
+</body>
+​
+</html>
