528f5fa70e7aa10a8360e0d6b4dc7e944a2a0a43
Pavlos Kafritsas <pkafritsas@webo2.gr>
2020-01-24 17:32:09 +0200

    added cafetex user account (seeder)

--- database/seeds/UserSeeder.php
+++ database/seeds/UserSeeder.php
@@ -60,5 +60,16 @@ class UserSeeder extends Seeder
         $user = User::whereEmail('cgidersos@webo2.gr')->first();
         $user->assignRole('admin');
         $user->save();
+
+        DB::table('users')->insert([
+            'name' => 'Cafetex',
+            'username' => 'cafetex',
+            'email' => 'cafetex@cafetex.gr',
+            'phone' => '1231231230',
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi',
+        ]);
+        $user = User::whereEmail('cafetex@cafetex.gr')->first();
+        $user->assignRole('admin');
+        $user->save();
     }
 }
