a5b2d04e785adb19c11058b55d92d6e9ee984bcf
Nikos Papaioannou <nikoskalmah@hotmail.com>
2020-01-29 12:53:18 -0500

    gift card form updated

--- resources/js/components/forms/GiftCardToCartForm.vue
+++ resources/js/components/forms/GiftCardToCartForm.vue
@@ -53,6 +53,10 @@
 				no-resize
 			></v-textarea>
 		</ValidationProvider>
+		<v-alert v-if="giftCardEnabled" dense outlined type="warning">
+			This gift card with {{giftCardEnabledAmount }} $ is enabled.
+			If you want to recharge it type the amount and add it to cart
+		</v-alert>
 		<v-btn class="mr-4" type="submit" :disabled="invalid">Add to cart</v-btn>
 	</ValidationObserver>
 </template>
@@ -61,6 +65,8 @@
 export default {
 	data() {
 		return {
+			giftCardEnabledAmount: null,
+			giftCardEnabled: false,
 			giftCardAmountDisabled: true,
 			giftCard: {
 				id: null,
@@ -93,10 +99,16 @@ export default {
 				if (response[0]) {
 					this.giftCard.name = response[0].name;
 					this.giftCard.code = response[0].code;
+					this.giftCard.price.amount = response[0].amount;
 					if (response[0].amount == 0) {
 						this.giftCardAmountDisabled = false;
 					}
-					this.giftCard.price.amount = response[0].amount;
+					if (response[0].enabled) {
+						this.giftCardAmountDisabled = false;
+						this.giftCardEnabled = true;
+						this.giftCard.price.amount = 0;
+						this.giftCardEnabledAmount = response[0].amount;
+					}
 				}
 			});
 		},
