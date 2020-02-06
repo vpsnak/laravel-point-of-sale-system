ad7511d5a52e15025f71311e3c8f335c33aac7a0
Pavlos Kafritsas <pavkfr@hotmail.com>
2020-02-06 12:29:50 -0500

    fixed mas mode detection

--- resources/js/components/menu/TopMenu.vue
+++ resources/js/components/menu/TopMenu.vue
@@ -1,233 +1,267 @@
 <template>
-	<v-app-bar app clipped-left>
-		<v-app-bar-nav-icon @click.stop="toggle = !toggle"></v-app-bar-nav-icon>
-		<v-toolbar-title>{{ app_name }}</v-toolbar-title>
+    <v-app-bar app clipped-left>
+        <v-app-bar-nav-icon @click.stop="toggle = !toggle"></v-app-bar-nav-icon>
+        <v-toolbar-title>{{ app_name }}</v-toolbar-title>
 
-		<v-spacer></v-spacer>
+        <v-spacer></v-spacer>
 
-		<v-toolbar-title class="pr-2">
-			Environment:
-			<i :class="txtColor(app_env)">{{ app_env }}</i>
-		</v-toolbar-title>
+        <v-toolbar-title class="pr-2">
+            Environment:
+            <i :class="txtColor(app_env)">{{ app_env }}</i>
+        </v-toolbar-title>
 
-		<v-spacer></v-spacer>
+        <v-spacer></v-spacer>
 
-		<v-toolbar-title class="pr-2">
-			MAS Mode:
-			<i
-				:class="txtColor(mas_production_mode ? 'production' : 'test')"
-			>{{ mas_production_mode ? 'production' : 'test' }}</i>
-		</v-toolbar-title>
+        <v-toolbar-title class="pr-2">
+            MAS Mode:
+            <i :class="txtColor(masMode)">{{ masMode }}</i>
+        </v-toolbar-title>
 
-		<v-spacer></v-spacer>
+        <v-spacer></v-spacer>
 
-		<v-chip
-			v-if="!openedRegister"
-			text
-			@click="$router.push({ name: 'openCashRegister' })"
-		>Select cash register</v-chip>
-		<v-menu v-if="openedRegister" left bottom offset-x transition="scale-transition">
-			<template v-slot:activator="{ on }">
-				<v-btn v-on="on" icon>
-					<v-avatar color="green" size="36">
-						<v-icon dark>mdi-cash-register</v-icon>
-					</v-avatar>
-				</v-btn>
-			</template>
-			<v-list dense>
-				<v-list-item-group>
-					<v-list-item inactive two-line @click.stop :ripple="false">
-						<v-list-item-avatar color="green">
-							<v-icon>mdi-cash-register</v-icon>
-						</v-list-item-avatar>
-						<v-list-item-content>
-							<v-list-item-title>{{ store.name }}</v-list-item-title>
-							<v-list-item-subtitle>{{ cashRegister.name }}</v-list-item-subtitle>
-						</v-list-item-content>
-					</v-list-item>
-					<v-divider />
-					<v-list-item @click="checkCashRegisterDialog">
-						<v-list-item-avatar>
-							<v-icon color="secondary">mdi-alpha-x-circle</v-icon>
-						</v-list-item-avatar>
-						<v-list-item-content>
-							<v-list-item-title>Generate X report</v-list-item-title>
-						</v-list-item-content>
-					</v-list-item>
-					<v-list-item @click="cashRegisterLogout()">
-						<v-list-item-avatar>
-							<v-icon color="secondary">mdi-logout</v-icon>
-						</v-list-item-avatar>
-						<v-list-item-content>
-							<v-list-item-title>Logout</v-list-item-title>
-						</v-list-item-content>
-					</v-list-item>
-					<v-divider></v-divider>
-					<v-list-item @click="closeCashRegisterDialog()">
-						<v-list-item-avatar>
-							<v-icon color="red">mdi-alpha-z-circle</v-icon>
-						</v-list-item-avatar>
-						<v-list-item-content>
-							<v-list-item-title>Close and generate Z report</v-list-item-title>
-						</v-list-item-content>
-					</v-list-item>
-				</v-list-item-group>
-			</v-list>
-		</v-menu>
-		<v-menu left bottom offset-x transition="scale-transition">
-			<template v-slot:activator="{ on }">
-				<v-btn v-on="on" icon>
-					<v-icon>mdi-account-circle</v-icon>
-				</v-btn>
-			</template>
-			<v-list dense>
-				<v-list-item-group>
-					<v-list-item inactive two-line @click.stop :ripple="false">
-						<v-list-item-avatar color="orange">{{ user.name.charAt(0) }}</v-list-item-avatar>
-						<v-list-item-content>
-							<v-list-item-title>{{ user.name }}</v-list-item-title>
-							<v-list-item-subtitle>{{ user.email }}</v-list-item-subtitle>
-						</v-list-item-content>
-					</v-list-item>
-					<v-divider />
-					<v-list-item to="/logout">
-						<v-list-item-avatar>
-							<v-icon>mdi-logout-variant</v-icon>
-						</v-list-item-avatar>
-						<v-list-item-content>
-							<v-list-item-title>Sign out</v-list-item-title>
-						</v-list-item-content>
-					</v-list-item>
-					<v-list-item @click="changePasswordDialog">
-						<v-list-item-avatar>
-							<v-icon>mdi-key</v-icon>
-						</v-list-item-avatar>
-						<v-list-item-content>
-							<v-list-item-title>Change password</v-list-item-title>
-						</v-list-item-content>
-					</v-list-item>
-					<v-divider />
-					<v-list-item @click.stop="darkMode = !darkMode">
-						<v-list-item-avatar>
-							<v-icon>mdi-brightness-4</v-icon>
-						</v-list-item-avatar>
-						<v-list-item-content>
-							<v-list-item-title>Dark mode</v-list-item-title>
-						</v-list-item-content>
-						<v-list-item-action>
-							<v-switch v-model="darkMode" @click.stop="darkMode = !darkMode"></v-switch>
-						</v-list-item-action>
-					</v-list-item>
-				</v-list-item-group>
-			</v-list>
-		</v-menu>
-	</v-app-bar>
+        <v-chip
+            v-if="!openedRegister"
+            text
+            @click="$router.push({ name: 'openCashRegister' })"
+            >Select cash register</v-chip
+        >
+        <v-menu
+            v-if="openedRegister"
+            left
+            bottom
+            offset-x
+            transition="scale-transition"
+        >
+            <template v-slot:activator="{ on }">
+                <v-btn v-on="on" icon>
+                    <v-avatar color="green" size="36">
+                        <v-icon dark>mdi-cash-register</v-icon>
+                    </v-avatar>
+                </v-btn>
+            </template>
+            <v-list dense>
+                <v-list-item-group>
+                    <v-list-item inactive two-line @click.stop :ripple="false">
+                        <v-list-item-avatar color="green">
+                            <v-icon>mdi-cash-register</v-icon>
+                        </v-list-item-avatar>
+                        <v-list-item-content>
+                            <v-list-item-title>{{
+                                store.name
+                            }}</v-list-item-title>
+                            <v-list-item-subtitle>{{
+                                cashRegister.name
+                            }}</v-list-item-subtitle>
+                        </v-list-item-content>
+                    </v-list-item>
+                    <v-divider />
+                    <v-list-item @click="checkCashRegisterDialog">
+                        <v-list-item-avatar>
+                            <v-icon color="secondary"
+                                >mdi-alpha-x-circle</v-icon
+                            >
+                        </v-list-item-avatar>
+                        <v-list-item-content>
+                            <v-list-item-title
+                                >Generate X report</v-list-item-title
+                            >
+                        </v-list-item-content>
+                    </v-list-item>
+                    <v-list-item @click="cashRegisterLogout()">
+                        <v-list-item-avatar>
+                            <v-icon color="secondary">mdi-logout</v-icon>
+                        </v-list-item-avatar>
+                        <v-list-item-content>
+                            <v-list-item-title>Logout</v-list-item-title>
+                        </v-list-item-content>
+                    </v-list-item>
+                    <v-divider></v-divider>
+                    <v-list-item @click="closeCashRegisterDialog()">
+                        <v-list-item-avatar>
+                            <v-icon color="red">mdi-alpha-z-circle</v-icon>
+                        </v-list-item-avatar>
+                        <v-list-item-content>
+                            <v-list-item-title
+                                >Close and generate Z report</v-list-item-title
+                            >
+                        </v-list-item-content>
+                    </v-list-item>
+                </v-list-item-group>
+            </v-list>
+        </v-menu>
+        <v-menu left bottom offset-x transition="scale-transition">
+            <template v-slot:activator="{ on }">
+                <v-btn v-on="on" icon>
+                    <v-icon>mdi-account-circle</v-icon>
+                </v-btn>
+            </template>
+            <v-list dense>
+                <v-list-item-group>
+                    <v-list-item inactive two-line @click.stop :ripple="false">
+                        <v-list-item-avatar color="orange">{{
+                            user.name.charAt(0)
+                        }}</v-list-item-avatar>
+                        <v-list-item-content>
+                            <v-list-item-title>{{
+                                user.name
+                            }}</v-list-item-title>
+                            <v-list-item-subtitle>{{
+                                user.email
+                            }}</v-list-item-subtitle>
+                        </v-list-item-content>
+                    </v-list-item>
+                    <v-divider />
+                    <v-list-item to="/logout">
+                        <v-list-item-avatar>
+                            <v-icon>mdi-logout-variant</v-icon>
+                        </v-list-item-avatar>
+                        <v-list-item-content>
+                            <v-list-item-title>Sign out</v-list-item-title>
+                        </v-list-item-content>
+                    </v-list-item>
+                    <v-list-item @click="changePasswordDialog">
+                        <v-list-item-avatar>
+                            <v-icon>mdi-key</v-icon>
+                        </v-list-item-avatar>
+                        <v-list-item-content>
+                            <v-list-item-title
+                                >Change password</v-list-item-title
+                            >
+                        </v-list-item-content>
+                    </v-list-item>
+                    <v-divider />
+                    <v-list-item @click.stop="darkMode = !darkMode">
+                        <v-list-item-avatar>
+                            <v-icon>mdi-brightness-4</v-icon>
+                        </v-list-item-avatar>
+                        <v-list-item-content>
+                            <v-list-item-title>Dark mode</v-list-item-title>
+                        </v-list-item-content>
+                        <v-list-item-action>
+                            <v-switch
+                                v-model="darkMode"
+                                @click.stop="darkMode = !darkMode"
+                            ></v-switch>
+                        </v-list-item-action>
+                    </v-list-item>
+                </v-list-item-group>
+            </v-list>
+        </v-menu>
+    </v-app-bar>
 </template>
 
 <script>
 import { mapState } from "vuex";
 
 export default {
-	methods: {
-		invertTheme() {
-			this.$vuetify.theme.dark = !this.$vuetify.theme.dark;
-		},
-		cashRegisterLogout() {
-			this.$store.dispatch("logoutCashRegister");
-		},
+    methods: {
+        invertTheme() {
+            this.$vuetify.theme.dark = !this.$vuetify.theme.dark;
+        },
+        cashRegisterLogout() {
+            this.$store.dispatch("logoutCashRegister");
+        },
 
-		dialogEvent(event) {
-			if (event) {
-				switch (this.action) {
-					case "closeCashRegister":
-						this.displayZDialog(event.response.report);
-						break;
-					default:
-						break;
-				}
-			}
-		},
-		txtColor(mode) {
-			switch (mode) {
-				case "local":
-				case "test":
-				case "development":
-					return "orange--text";
-				case "live":
-				case "production":
-					return "green--text";
-				case true:
-				default:
-					return "white--text";
-			}
-		},
-		closeCashRegisterDialog() {
-			this.dialog = {
-				width: 600,
-				title: `Close and generate Z report`,
-				titleCloseBtn: true,
-				icon: "mdi-alpha-z-circle",
-				component: "closeCashRegisterForm",
-				persistent: true
-			};
-		},
-		changePasswordDialog() {
-			this.dialog = {
-				width: 600,
-				title: `Change Password`,
-				titleCloseBtn: true,
-				model: { action: "change_self" },
-				icon: "mdi-key",
-				component: "PasswordForm",
-				persistent: true
-			};
-		},
-		displayZDialog(event) {
-			this.dialog = {
-				width: 1000,
-				title: event.report_name,
-				titleCloseBtn: true,
-				model: event,
-				icon: "mdi-alpha-z-circle",
-				component: "cashRegisterReports",
-				persistent: true
-			};
-		},
-		checkCashRegisterDialog() {
-			this.dialog = {
-				width: 1000,
-				title: "Report: " + this.cashRegister.name,
-				titleCloseBtn: true,
-				icon: "mdi-alpha-x-circle",
-				component: "cashRegisterReports",
-				persistent: true
-			};
-		}
-	},
+        dialogEvent(event) {
+            if (event) {
+                switch (this.action) {
+                    case "closeCashRegister":
+                        this.displayZDialog(event.response.report);
+                        break;
+                    default:
+                        break;
+                }
+            }
+        },
+        txtColor(mode) {
+            switch (mode) {
+                case "local":
+                case "test":
+                case "development":
+                    return "orange--text";
+                case "live":
+                case "production":
+                    return "green--text";
+                case true:
+                default:
+                    return "white--text";
+            }
+        },
+        closeCashRegisterDialog() {
+            this.dialog = {
+                width: 600,
+                title: `Close and generate Z report`,
+                titleCloseBtn: true,
+                icon: "mdi-alpha-z-circle",
+                component: "closeCashRegisterForm",
+                persistent: true
+            };
+        },
+        changePasswordDialog() {
+            this.dialog = {
+                width: 600,
+                title: `Change Password`,
+                titleCloseBtn: true,
+                model: { action: "change_self" },
+                icon: "mdi-key",
+                component: "PasswordForm",
+                persistent: true
+            };
+        },
+        displayZDialog(event) {
+            this.dialog = {
+                width: 1000,
+                title: event.report_name,
+                titleCloseBtn: true,
+                model: event,
+                icon: "mdi-alpha-z-circle",
+                component: "cashRegisterReports",
+                persistent: true
+            };
+        },
+        checkCashRegisterDialog() {
+            this.dialog = {
+                width: 1000,
+                title: "Report: " + this.cashRegister.name,
+                titleCloseBtn: true,
+                icon: "mdi-alpha-x-circle",
+                component: "cashRegisterReports",
+                persistent: true
+            };
+        }
+    },
 
-	computed: {
-		...mapState("config", ["app_env", "app_name", "mas_production_mode"]),
-		...mapState(["user", "cashRegister", "store"]),
+    computed: {
+        ...mapState("config", ["app_env", "app_name", "mas_production_mode"]),
+        ...mapState(["user", "cashRegister", "store"]),
 
-		openedRegister() {
-			return this.$store.getters.openedRegister;
-		},
-		darkMode: {
-			get() {
-				return this.$vuetify.theme.dark;
-			},
-			set(value) {
-				this.$vuetify.theme.dark = value;
-			}
-		},
-		toggle: {
-			get() {
-				return this.$store.state.menu.toggle;
-			},
-			set(value) {
-				this.$store.commit("menu/toggleMenu", value);
-			}
-		}
-	}
+        masMode() {
+            console.log(this.mas_production_mode);
+            if (this.mas_production_mode === "true") {
+                return "production";
+            } else {
+                return "test";
+            }
+        },
+        openedRegister() {
+            return this.$store.getters.openedRegister;
+        },
+        darkMode: {
+            get() {
+                return this.$vuetify.theme.dark;
+            },
+            set(value) {
+                this.$vuetify.theme.dark = value;
+            }
+        },
+        toggle: {
+            get() {
+                return this.$store.state.menu.toggle;
+            },
+            set(value) {
+                this.$store.commit("menu/toggleMenu", value);
+            }
+        }
+    }
 };
 </script>
