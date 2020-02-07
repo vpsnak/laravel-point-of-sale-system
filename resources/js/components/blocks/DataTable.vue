e49bc8d2bff1be9fc28aafefb5a1a0a84e99e0b6
Pavlos Kafritsas <pavkfr@hotmail.com>
2020-02-07 09:48:02 -0500

    formatting using 2 indents instead of 4, fixed barcode scanner @ sales page

--- resources/js/components/blocks/DataTable.vue
+++ resources/js/components/blocks/DataTable.vue
@@ -1,74 +1,65 @@
 <template>
-    <v-container fluid>
-        <v-card>
-            <v-card-title>
-                <v-icon v-if="$props.icon" class="mr-2">{{
-                    $props.icon
-                }}</v-icon>
-                {{ $props.title }}
-                <v-spacer></v-spacer>
-                <v-text-field
-                    :disabled="loading"
-                    :search="search"
-                    prepend-icon="search"
-                    hide-details
-                    label="Search"
-                    single-line
-                    v-model="keyword"
-                    append-icon="mdi-close"
-                    @click:append="
-                        (keyword = null),
-                            (searchAction = null),
-                            paginate($event)
-                    "
-                    @click:prepend="search"
-                    @keyup.enter="search"
-                ></v-text-field>
-                <v-divider
-                    class="mx-4"
-                    v-if="$props.newForm && $props.btnTxt"
-                    inset
-                    vertical
-                ></v-divider>
-                <v-btn
-                    v-if="$props.newForm && $props.btnTxt"
-                    :disabled="$props.disableNewBtn || loading"
-                    color="primary"
-                    @click="createItemDialog"
-                    >{{ $props.btnTxt }}</v-btn
-                >
-            </v-card-title>
+  <v-container fluid>
+    <v-card>
+      <v-card-title>
+        <v-icon v-if="$props.icon" class="mr-2">
+          {{ $props.icon }}
+        </v-icon>
+        {{ $props.title }}
+        <v-spacer></v-spacer>
+        <v-text-field
+          :disabled="loading"
+          :search="search"
+          prepend-icon="search"
+          hide-details
+          label="Search"
+          single-line
+          v-model="keyword"
+          append-icon="mdi-close"
+          @click:append="
+            (keyword = null), (search_action = null), paginate($event)
+          "
+          @click:prepend="search"
+          @keyup.enter="search"
+        ></v-text-field>
+        <v-divider
+          class="mx-4"
+          v-if="$props.newForm && $props.btnTxt"
+          inset
+          vertical
+        ></v-divider>
+        <v-btn
+          v-if="$props.newForm && $props.btnTxt"
+          :disabled="$props.disableNewBtn || loading"
+          color="primary"
+          @click="createItemDialog"
+          >{{ $props.btnTxt }}</v-btn
+        >
+      </v-card-title>
 
-            <v-data-table
-                disable-sort
-                dense
-                :disable-filtering="true"
-                :headers="$props.headers"
-                :items="rows"
-                :loading="loading"
-                :items-per-page="15"
-                :page.sync="currentPage"
-                :server-items-length="totalItems"
-                @pagination="paginate"
-                :footer-props="footerProps"
-            >
-                <template
-                    v-for="(_, slot) of $scopedSlots"
-                    v-slot:[slot]="scope"
-                >
-                    <slot :name="slot" v-bind="scope" />
-                </template>
+      <v-data-table
+        disable-sort
+        dense
+        :disable-filtering="true"
+        :headers="$props.headers"
+        :items="rows"
+        :loading="loading"
+        :items-per-page="15"
+        :page.sync="currentPage"
+        :server-items-length="totalItems"
+        @pagination="paginate"
+        :footer-props="footerProps"
+      >
+        <template v-for="(_, slot) of $scopedSlots" v-slot:[slot]="scope">
+          <slot :name="slot" v-bind="scope" />
+        </template>
 
-                <v-alert
-                    :value="true"
-                    color="error"
-                    icon="warning"
-                    slot="no-results"
-                    >Your search for "{{ keyword }}" found no results.</v-alert
-                >
-            </v-data-table>
-        </v-card>
-    </v-container>
+        <v-alert :value="true" color="error" icon="warning" slot="no-results"
+          >Your search for "{{ keyword }}" found no results.</v-alert
+        >
+      </v-data-table>
+    </v-card>
+  </v-container>
 </template>
 
 <script>
@@ -76,178 +67,171 @@ import { mapActions, mapMutations, mapState } from "vuex";
 import { EventBus } from "../../plugins/event-bus";
 
 export default {
-    data() {
-        return {
-            current_page: 1,
-            total_items: null,
-            action: "",
-            keyword: "",
-            search_action: false
-        };
-    },
-    props: {
-        icon: String,
-        title: String,
-        headers: Array,
-        dataUrl: String,
-        btnTxt: String,
-        newForm: String,
-        disableNewBtn: Boolean
-    },
-    mounted() {
-        EventBus.$on("data-table", event => {
-            console.log(event);
+  data() {
+    return {
+      current_page: 1,
+      total_items: null,
+      action: "",
+      keyword: "",
+      search_action: false
+    };
+  },
+  props: {
+    icon: String,
+    title: String,
+    headers: Array,
+    dataUrl: String,
+    btnTxt: String,
+    newForm: String,
+    disableNewBtn: Boolean
+  },
+  mounted() {
+    EventBus.$on("data-table", event => {
+      console.log(event);
 
-            switch (event.action) {
-                case "paginate":
-                    this.paginate();
-                    break;
-                case "search":
-                    this.keyword = event.keyword || null;
-                    this.search();
-                default:
-                    if (_.isBoolean(event.payload) && event.payload) {
-                        this.paginate();
-                    }
-                    break;
-            }
-        });
+      switch (event.action) {
+        case "paginate":
+          this.paginate();
+          break;
+        case "search":
+          this.keyword = event.keyword || null;
+          this.search();
+          break;
+        default:
+          if (_.isBoolean(event.payload) && event.payload) {
+            this.paginate();
+          }
+          break;
+      }
+    });
 
-        if (this.$props.tableForm === "productForm") {
-            this.$root.$on("barcodeScan", sku => {
-                this.keyword = sku;
-                this.search();
-            });
-        }
-    },
-    beforeDestroy() {
-        EventBus.$off();
-        if (this.$props.tableForm === "productForm") {
-            this.$root.$off("barcodeScan");
-        }
-    },
-    computed: {
-        searchAction: {
-            get() {
-                return this.search_action;
-            },
-            set(value) {
-                this.search_action = value;
-            }
-        },
-        footerProps() {
-            return {
-                "disable-pagination": this.loading,
-                "disable-items-per-page": true,
-                options: { itemsPerPage: 15, page: this.currentPage }
-            };
-        },
-        totalItems: {
-            get() {
-                return this.total_items;
-            },
-            set(value) {
-                this.total_items = value;
-            }
-        },
-        currentPage: {
-            get() {
-                return this.current_page;
-            },
-            set(value) {
-                this.current_page = value;
-            }
-        },
-        ...mapState("datatable", {
-            rows: "rows",
-            loading: "loading"
-        })
-    },
-    methods: {
-        ...mapMutations("dialog", ["setDialog"]),
-
-        search(e, page = false) {
-            if (this.keyword.length > 2 || this.searchAction) {
-                this.setLoading(true);
-
-                if (!page) {
-                    this.searchAction = this.keyword;
-                } else {
-                    if (!this.keyword) {
-                        this.keyword = this.searchAction;
-                    }
-                }
-
-                let payload = {
-                    model: this.dataUrl,
-                    page: page || 1,
-                    keyword: this.keyword,
-                    dataTable: true
-                };
-
-                this.searchRequest(payload)
-                    .then(response => {
-                        this.setRows(response.data);
-
-                        if (response.total !== this.totalItems) {
-                            this.totalItems = response.total;
-                        }
-                    })
-                    .finally(() => {
-                        this.setLoading(false);
-                    });
-            }
-        },
-
-        paginate(event) {
-            this.setRows([]);
-
-            if (this.searchAction) {
-                this.search(null, event.page);
-            } else {
-                this.searchAction = false;
-
-                this.setLoading(true);
-
-                this.getAll({
-                    model: this.dataUrl,
-                    page: event ? event.page : this.currentPage,
-                    dataTable: true
-                })
-                    .then(response => {
-                        this.setRows(response.data);
-
-                        if (response.total !== this.totalItems) {
-                            this.totalItems = response.total;
-                        }
-                    })
-                    .catch(() => {})
-                    .finally(() => {
-                        this.setLoading(false);
-                    });
-            }
-        },
-
-        createItemDialog() {
-            this.setDialog = {
-                show: true,
-                width: 600,
-                title: this.$props.btnTxt,
-                titleCloseBtn: true,
-                component: this.$props.newForm,
-                persistent: true
-            };
-        },
-
-        ...mapMutations("datatable", {
-            setRows: "setRows",
-            setLoading: "setLoading"
-        }),
-
-        ...mapActions({
-            getAll: "getAll",
-            searchRequest: "search"
-        })
+    if (this.$props.tableForm === "productForm") {
+      this.$root.$on("barcodeScan", sku => {
+        this.keyword = sku;
+        this.search();
+      });
     }
+  },
+  beforeDestroy() {
+    EventBus.$off();
+    if (this.$props.tableForm === "productForm") {
+      this.$root.$off("barcodeScan");
+    }
+  },
+  computed: {
+    footerProps() {
+      return {
+        "disable-pagination": this.loading,
+        "disable-items-per-page": true,
+        options: { itemsPerPage: 15, page: this.currentPage }
+      };
+    },
+    totalItems: {
+      get() {
+        return this.total_items;
+      },
+      set(value) {
+        this.total_items = value;
+      }
+    },
+    currentPage: {
+      get() {
+        return this.current_page;
+      },
+      set(value) {
+        this.current_page = value;
+      }
+    },
+    ...mapState("datatable", {
+      rows: "rows",
+      loading: "loading"
+    })
+  },
+  methods: {
+    ...mapMutations("dialog", ["setDialog"]),
+
+    search(e, page = false) {
+      if (this.keyword.length > 2 || this.search_action) {
+        this.setLoading(true);
+
+        if (!page) {
+          this.search_action = this.keyword;
+        } else {
+          if (!this.keyword) {
+            this.keyword = this.search_action;
+          }
+        }
+
+        let payload = {
+          model: this.dataUrl,
+          page: page || 1,
+          keyword: this.keyword,
+          dataTable: true
+        };
+
+        this.searchRequest(payload)
+          .then(response => {
+            this.setRows(response.data);
+
+            if (response.total !== this.totalItems) {
+              this.totalItems = response.total;
+            }
+          })
+          .finally(() => {
+            this.setLoading(false);
+          });
+      }
+    },
+
+    paginate(event) {
+      this.setRows([]);
+
+      if (this.search_action) {
+        this.search(null, event.page);
+      } else {
+        this.search_action = false;
+
+        this.setLoading(true);
+
+        this.getAll({
+          model: this.dataUrl,
+          page: event ? event.page : this.currentPage,
+          dataTable: true
+        })
+          .then(response => {
+            this.setRows(response.data);
+
+            if (response.total !== this.totalItems) {
+              this.totalItems = response.total;
+            }
+          })
+          .catch(() => {})
+          .finally(() => {
+            this.setLoading(false);
+          });
+      }
+    },
+
+    createItemDialog() {
+      this.setDialog = {
+        show: true,
+        width: 600,
+        title: this.$props.btnTxt,
+        titleCloseBtn: true,
+        component: this.$props.newForm,
+        persistent: true
+      };
+    },
+
+    ...mapMutations("datatable", {
+      setRows: "setRows",
+      setLoading: "setLoading"
+    }),
+
+    ...mapActions({
+      getAll: "getAll",
+      searchRequest: "search"
+    })
+  }
 };
 </script>
--- resources/js/components/checkout/payment/PaymentHistory.vue
+++ resources/js/components/checkout/payment/PaymentHistory.vue
@@ -1,53 +1,47 @@
 <template>
-    <div>
-        <v-col :cols="12">
-            <h3 class="mb-2">Payment history</h3>
-        </v-col>
+  <div>
+    <v-col :cols="12">
+      <h3 class="mb-2">Payment history</h3>
+    </v-col>
 
-        <v-col :cols="12">
-            <v-data-table
-                :headers="headers"
-                :items="paymentHistory"
-                class="elevation-1"
-                disable-pagination
-                disable-filtering
-                hide-default-footer
+    <v-col :cols="12">
+      <v-data-table
+        :headers="headers"
+        :items="paymentHistory"
+        class="elevation-1"
+        disable-pagination
+        disable-filtering
+        hide-default-footer
+        :loading="loading || refundLoading || paymentHistoryLoading"
+      >
+        <template v-slot:item.status="{ item }">
+          <span :class="statusColor(item.status)">
+            <b>{{ parseStatus(item.status) }}</b>
+          </span>
+        </template>
+
+        <template v-slot:item.actions="{ item }">
+          <v-tooltip bottom>
+            <template v-slot:activator="{ on }">
+              <v-btn
+                @click="refundDialog(item)"
+                icon
+                v-on="on"
                 :loading="loading || refundLoading || paymentHistoryLoading"
-            >
-                <template v-slot:item.status="{ item }">
-                    <span :class="statusColor(item.status)">
-                        <b>{{ parseStatus(item.status) }}</b>
-                    </span>
-                </template>
-
-                <template v-slot:item.actions="{ item }">
-                    <v-tooltip bottom>
-                        <template v-slot:activator="{ on }">
-                            <v-btn
-                                @click="refundDialog(item)"
-                                icon
-                                v-on="on"
-                                :loading="
-                                    loading ||
-                                        refundLoading ||
-                                        paymentHistoryLoading
-                                "
-                                v-if="
-                                    item.status === 'approved' && !item.refunded
-                                "
-                            >
-                                <v-icon v-if="item.payment_type.type === 'cash'"
-                                    >mdi-cash-refund</v-icon
-                                >
-                                <v-icon v-else>mdi-credit-card-refund</v-icon>
-                            </v-btn>
-                        </template>
-                        <span>Refund</span>
-                    </v-tooltip>
-                </template>
-            </v-data-table>
-        </v-col>
-    </div>
+                v-if="item.status === 'approved' && !item.refunded"
+              >
+                <v-icon v-if="item.payment_type.type === 'cash'"
+                  >mdi-cash-refund</v-icon
+                >
+                <v-icon v-else>mdi-credit-card-refund</v-icon>
+              </v-btn>
+            </template>
+            <span>Refund</span>
+          </v-tooltip>
+        </template>
+      </v-data-table>
+    </v-col>
+  </div>
 </template>
 
 <script>
@@ -55,163 +49,164 @@ import { mapActions, mapState, mapMutations } from "vuex";
 import { EventBus } from "../../../plugins/event-bus";
 
 export default {
-    mounted() {
-        EventBus.$on("payment-history-refund", event => {
-            console.log("asd");
-            console.log(event);
+  mounted() {
+    console.log("asd");
+    EventBus.$on("payment-history-refund", event => {
+      console.log("asd");
+      console.log(event);
 
-            if (event.payload) {
-                this.refund();
-            }
+      if (event.payload) {
+        this.refund();
+      }
+    });
+  },
+
+  beforeDestroy() {
+    this.$off("refund");
+    EventBus.$off();
+  },
+
+  props: {
+    loading: Boolean
+  },
+
+  data() {
+    return {
+      paymentHistoryLoading: false,
+      paymentID: null,
+      headers: [
+        {
+          text: "Payment ID",
+          value: "id",
+          sortable: false
+        },
+        {
+          text: "Operator",
+          value: "created_by.name",
+          sortable: false
+        },
+        {
+          text: "Date",
+          value: "created_at",
+          sortable: false
+        },
+        {
+          text: "Type",
+          value: "payment_type.name",
+          sortable: false
+        },
+        {
+          text: "Status",
+          value: "status",
+          sortable: false
+        },
+        {
+          text: "Amount (USD)",
+          value: "amount",
+          sortable: false
+        },
+        {
+          text: "Actions",
+          value: "actions"
+        }
+      ]
+    };
+  },
+  computed: {
+    ...mapState("cart", ["order"]),
+
+    paymentHistory: {
+      get() {
+        return this.order.payments;
+      },
+      set(value) {
+        this.$store.commit("cart/setPaymentHistory", value);
+      }
+    },
+    orderId() {
+      return this.order.id;
+    },
+    refundLoading: {
+      get() {
+        return this.$store.state.cart.refundLoading;
+      },
+      set(value) {
+        this.$store.state.cart.refundLoading = value;
+      }
+    }
+  },
+  methods: {
+    ...mapActions(["search", "delete"]),
+    ...mapMutations("dialog", ["setDialog"]),
+
+    parseStatus(status) {
+      return _.upperFirst(status);
+    },
+    statusColor(status) {
+      switch (status) {
+        case "approved":
+          return "green--text";
+        case "failed":
+          return "red--text";
+        case "refunded":
+          return "orange--text";
+        default:
+          return null;
+      }
+    },
+    getPaymentHistory() {
+      if (this.orderId) {
+        this.paymentHistoryLoading = true;
+
+        let payload = {
+          model: "payments",
+          keyword: this.orderId
+        };
+        this.search(payload)
+          .then(response => {
+            this.paymentHistory = response;
+          })
+          .finally(() => {
+            this.paymentHistoryLoading = false;
+          });
+      }
+    },
+    refund() {
+      this.refundLoading = true;
+      let payload = {
+        model: "payments",
+        id: this.paymentID
+      };
+
+      this.delete(payload)
+        .then(response => {
+          this.$emit("refund", response);
+        })
+        .catch(error => {
+          if (error.response.data.refund) {
+            this.payments = error.response.data.refund;
+          }
+
+          this.$emit("refund", error.response.data);
         });
     },
 
-    beforeDestroy() {
-        this.$off("refund");
-        EventBus.$off("payment-history-refund");
-    },
+    refundDialog(item) {
+      const dialog = {
+        width: 600,
+        title: `Verify your password to refund payment #${item.id}`,
+        titleCloseBtn: true,
+        icon: "mdi-lock-alert",
+        component: "passwordForm",
+        model: { action: "verify" },
+        persistent: true,
+        eventChannel: "payment-history-refund"
+      };
 
-    props: {
-        loading: Boolean
-    },
+      this.paymentID = item.id;
 
-    data() {
-        return {
-            paymentHistoryLoading: false,
-            paymentID: null,
-            headers: [
-                {
-                    text: "Payment ID",
-                    value: "id",
-                    sortable: false
-                },
-                {
-                    text: "Operator",
-                    value: "created_by.name",
-                    sortable: false
-                },
-                {
-                    text: "Date",
-                    value: "created_at",
-                    sortable: false
-                },
-                {
-                    text: "Type",
-                    value: "payment_type.name",
-                    sortable: false
-                },
-                {
-                    text: "Status",
-                    value: "status",
-                    sortable: false
-                },
-                {
-                    text: "Amount (USD)",
-                    value: "amount",
-                    sortable: false
-                },
-                {
-                    text: "Actions",
-                    value: "actions"
-                }
-            ]
-        };
-    },
-    computed: {
-        ...mapState("cart", ["order"]),
-
-        paymentHistory: {
-            get() {
-                return this.order.payments;
-            },
-            set(value) {
-                this.$store.commit("cart/setPaymentHistory", value);
-            }
-        },
-        orderId() {
-            return this.order.id;
-        },
-        refundLoading: {
-            get() {
-                return this.$store.state.cart.refundLoading;
-            },
-            set(value) {
-                this.$store.state.cart.refundLoading = value;
-            }
-        }
-    },
-    methods: {
-        ...mapActions(["search", "delete"]),
-        ...mapMutations("dialog", ["setDialog"]),
-
-        parseStatus(status) {
-            return _.upperFirst(status);
-        },
-        statusColor(status) {
-            switch (status) {
-                case "approved":
-                    return "green--text";
-                case "failed":
-                    return "red--text";
-                case "refunded":
-                    return "orange--text";
-                default:
-                    return null;
-            }
-        },
-        getPaymentHistory() {
-            if (this.orderId) {
-                this.paymentHistoryLoading = true;
-
-                let payload = {
-                    model: "payments",
-                    keyword: this.orderId
-                };
-                this.search(payload)
-                    .then(response => {
-                        this.paymentHistory = response;
-                    })
-                    .finally(() => {
-                        this.paymentHistoryLoading = false;
-                    });
-            }
-        },
-        refund() {
-            this.refundLoading = true;
-            let payload = {
-                model: "payments",
-                id: this.paymentID
-            };
-
-            this.delete(payload)
-                .then(response => {
-                    this.$emit("refund", response);
-                })
-                .catch(error => {
-                    if (error.response.data.refund) {
-                        this.payments = error.response.data.refund;
-                    }
-
-                    this.$emit("refund", error.response.data);
-                });
-        },
-
-        refundDialog(item) {
-            const dialog = {
-                width: 600,
-                title: `Verify your password to refund payment #${item.id}`,
-                titleCloseBtn: true,
-                icon: "mdi-lock-alert",
-                component: "passwordForm",
-                model: { action: "verify" },
-                persistent: true,
-                eventChannel: "payment-history-refund"
-            };
-
-            this.paymentID = item.id;
-
-            this.setDialog(dialog);
-        }
+      this.setDialog(dialog);
     }
+  }
 };
 </script>
