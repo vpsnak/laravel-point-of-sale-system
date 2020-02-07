28fb202288591f5c7fb723f80e2b5e3fca727701
nikpapaioan <npapaioannou@webo2.gr>
2020-02-07 10:03:18 -0500

    delete delivery fees from checkout

--- resources/js/components/checkout/stepper/ShippingStep.vue
+++ resources/js/components/checkout/stepper/ShippingStep.vue
@@ -1,52 +1,50 @@
 <template>
-	<div>
-		<shipping />
-		<v-row justify="center" align="center" class="my-3">
-			<v-col :cols="3" align="start" v-if="shipping.cost">
-				<span class="title">
-					Delivery Fees:
-					<span class="amber--text">$ {{ shipping.cost }}</span>
-				</span>
-			</v-col>
-			<v-col :cols="shipping.cost ? 3 : 6" justify="center" align="end">
-				<v-btn color="primary" :disabled="!isValid" @click="completeStep">
-					Next
-					<v-icon small right>mdi-chevron-right</v-icon>
-				</v-btn>
-			</v-col>
-		</v-row>
-	</div>
+    <div>
+        <shipping />
+        <v-row justify="center" align="center" class="my-3">
+            <v-col :cols="6" justify="center" align="end">
+                <v-btn
+                    color="primary"
+                    :disabled="!isValid"
+                    @click="completeStep"
+                >
+                    Next
+                    <v-icon small right>mdi-chevron-right</v-icon>
+                </v-btn>
+            </v-col>
+        </v-row>
+    </div>
 </template>
 
 <script>
 import { mapActions } from "vuex";
 
 export default {
-	props: {
-		currentStep: Object
-	},
+    props: {
+        currentStep: Object
+    },
 
-	computed: {
-		isValid() {
-			return this.$store.state.cart.isValid;
-		},
-		shipping: {
-			get() {
-				return this.$store.state.cart.shipping;
-			},
-			set(value) {
-				this.$store.state.cart.shipping = value;
-			}
-		}
-	},
+    computed: {
+        isValid() {
+            return this.$store.state.cart.isValid;
+        },
+        shipping: {
+            get() {
+                return this.$store.state.cart.shipping;
+            },
+            set(value) {
+                this.$store.state.cart.shipping = value;
+            }
+        }
+    },
 
-	methods: {
-		completeStep() {
-			this.$store.dispatch("cart/completeStep");
-		},
-		prevStep() {
-			this.$store.state.cart.currentCheckoutStep--;
-		}
-	}
+    methods: {
+        completeStep() {
+            this.$store.dispatch("cart/completeStep");
+        },
+        prevStep() {
+            this.$store.state.cart.currentCheckoutStep--;
+        }
+    }
 };
 </script>
