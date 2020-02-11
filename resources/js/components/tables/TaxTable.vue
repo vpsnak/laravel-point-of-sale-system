f777a624d8054118baadc87966f7299f8b2d6fcd
nikpapaioan <npapaioannou@webo2.gr>
2020-02-11 10:08:20 -0500

    tax table rework

--- resources/js/components/tables/TaxTable.vue
+++ resources/js/components/tables/TaxTable.vue
@@ -1,78 +1,74 @@
 <template>
-    <data-table
-        title="Taxes"
-        icon="mdi-sack-percent"
-        :headers="headers"
-        data-url="taxes"
-        btnTxt="New Tax"
-        :newForm="form"
-    >
-        <template v-slot:item.actions="{ item }">
-            <v-tooltip bottom>
-                <template v-slot:activator="{ on }">
-                    <v-btn
-                        small
-                        :disabled="disableActions"
-                        @click.stop="(item.form = form), editItem(item)"
-                        class="my-2"
-                        v-on="on"
-                        icon
-                    >
-                        <v-icon small>edit</v-icon>
-                    </v-btn>
-                </template>
-                <span>Edit</span>
-            </v-tooltip>
+	<data-table v-if="data_table.model">
+		<template v-slot:item.actions="{ item }">
+			<v-tooltip bottom>
+				<template v-slot:activator="{ on }">
+					<v-btn
+						small
+						:disabled="data_table.loading"
+						@click.stop="(item.form = form), editItem(item)"
+						class="my-2"
+						v-on="on"
+						icon
+					>
+						<v-icon small>edit</v-icon>
+					</v-btn>
+				</template>
+				<span>Edit</span>
+			</v-tooltip>
 
-            <v-tooltip bottom>
-                <template v-slot:activator="{ on }">
-                    <v-btn
-                        small
-                        :disabled="disableActions"
-                        @click.stop="(item.form = form), viewItem(item)"
-                        class="my-2"
-                        v-on="on"
-                        icon
-                    >
-                        <v-icon small>mdi-eye</v-icon>
-                    </v-btn>
-                </template>
-                <span>View</span>
-            </v-tooltip>
-        </template>
-    </data-table>
+			<v-tooltip bottom>
+				<template v-slot:activator="{ on }">
+					<v-btn
+						small
+						:disabled="data_table.loading"
+						@click.stop="(item.form = form), viewItem(item)"
+						class="my-2"
+						v-on="on"
+						icon
+					>
+						<v-icon small>mdi-eye</v-icon>
+					</v-btn>
+				</template>
+				<span>View</span>
+			</v-tooltip>
+		</template>
+	</data-table>
 </template>
 
 <script>
-import { mapState, mapMutations } from "vuex";
+import { mapMutations, mapActions, mapState } from "vuex";
 
 export default {
-    data() {
-        return {
-            form: "taxForm",
-            headers: [
-                { text: "#", value: "id" },
-                { text: "Name", value: "name" },
-                { text: "Percentage", value: "percentage" },
-                { text: "Actions", value: "actions" }
-            ]
-        };
-    },
-    computed: {
-        ...mapState("datatable", ["loading"]),
+	mounted() {
+		this.setDataTable({
+			icon: "mdi-sack-percent",
+			title: "Taxes",
+			headers: this.headers,
+			model: "taxes",
+			component: "taxForm",
+			loading: true
+		});
+	},
 
-        disableActions: {
-            get() {
-                return this.loading;
-            },
-            set(value) {
-                this.setLoading(value);
-            }
-        }
-    },
-    methods: {
-        ...mapMutations("dialog", ["editItem", "viewItem"]),
-        ...mapMutations("datatable", ["setLoading"])
-    }
+	data() {
+		return {
+			form: "taxForm",
+			headers: [
+				{ text: "#", value: "id" },
+				{ text: "Name", value: "name" },
+				{ text: "Percentage", value: "percentage" },
+				{ text: "Actions", value: "actions" }
+			]
+		};
+	},
+	computed: {
+		...mapState("datatable", ["data_table"])
+	},
+	methods: {
+		...mapMutations("dialog", ["editItem", "viewItem"]),
+		...mapMutations("datatable", ["setLoading", "setDataTable"]),
+		...mapActions(["getOne", "delete"])
+	}
 };
 </script>
