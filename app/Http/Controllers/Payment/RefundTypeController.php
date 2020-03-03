7459041947d00cab769341ea15f06e6a819e1fd1
Pavlos Kafritsas <pavkfr@hotmail.com>
2020-03-03 08:58:32 -0500

    updated

--- app/Http/Controllers/Payment/RefundTypeController.php
+++ app/Http/Controllers/Payment/RefundTypeController.php
@@ -11,15 +11,16 @@ class RefundTypeController extends Controller
     {
         $refundTypes = RefundType::enabled();
 
-        // $this->store = auth()->user()->open_register->cash_register->store;
-        // if ($this->store->is_phone_center) {
-        //     $phoneCenterPaymentTypes[0] = $paymentTypes[2];
-        //     $phoneCenterPaymentTypes[1] = $paymentTypes[3];
-        //     $phoneCenterPaymentTypes[2] = $paymentTypes[4];
-        //     $phoneCenterPaymentTypes[3] = $paymentTypes[5];
-        //     return response($phoneCenterPaymentTypes);
-        // } else {
-        //     return response($paymentTypes);
-        // }
+        // @TODO pick methods for phonecenters
+        $this->store = auth()->user()->open_register->cash_register->store;
+        if ($this->store->is_phone_center) {
+            //     $phoneCenterPaymentTypes[0] = $refundTypes[2];
+            //     $phoneCenterPaymentTypes[1] = $refundTypes[3];
+            //     $phoneCenterPaymentTypes[2] = $refundTypes[4];
+            //     $phoneCenterPaymentTypes[3] = $refundTypes[5];
+            //     return response($phoneCenterPaymentTypes);
+        } else {
+            return response($refundTypes);
+        }
     }
 }
