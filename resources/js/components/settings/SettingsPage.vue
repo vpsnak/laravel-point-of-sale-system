dfcbecc052fde1294fee62383ab0c9d87dfc966e
Pavlos Kafritsas <pavkfr@hotmail.com>
2020-03-13 21:39:39 -0400

    added settings page

---
+++ resources/js/components/settings/SettingsPage.vue
@@ -0,0 +1,13 @@
+<template>
+  <v-container>
+    <v-row>
+      <v-col :cols="3">
+        <v-card :to="{ name: 'masSettingsPage' }"> </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {};
+</script>
